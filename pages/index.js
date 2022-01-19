aa5e32ef3c2483ce7f6080684928fcecb7ad3922
Aaron Curtis <77994989+aaroncurtisyoga@users.noreply.github.com>
2022-01-18 20:18:19 -0500

    Render internal links using Next's Link component. DOnt show "Blog Series" link for now

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,19 @@
 import Head from "next/head";
+import LandingPage from "../src/components/landing-page/landing-page";
 
-export default function Home() {
+export default function HomePage() {
   return (
     <div className="container">
       <Head>
         <title>Aaron Curtis Yoga</title>
-        {/*todo: Change favicon */}
         <link rel="icon" href="/public/favicon.ico" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Fraunces:wght@300;400;600&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <main>
-        <p>main content</p>
+        <LandingPage />
       </main>
     </div>
   );
--- src/components/info/link-tree/link-tree.js
+++ src/components/info/link-tree/link-tree.js
@@ -1,14 +1,19 @@
 import classes from "./link-tree.module.scss";
+import Link from "next/link";
 
 export default function LinkTree() {
   return (
     <ul className={classes.linkTree}>
-      {links.map(({ title, url }) => {
+      {links.map(({ title, url, interal }) => {
         return (
           <li key={title} className={classes.link}>
-            <a href={url} target={`_blank`}>
-              {title}
-            </a>
+            {interal ? (
+              <Link href={url} />
+            ) : (
+              <a href={url} target={`_blank`}>
+                {title}
+              </a>
+            )}
           </li>
         );
       })}
@@ -21,20 +26,24 @@ const links = [
     title: "find a yoga class",
     url: "https://clients.mindbodyonline.com/classic/mainclass?studioid=2070",
     desc: "I teach at Yoga District. Hop over to their scheduling site to sign up for a class with me :) ",
+    internal: false,
   },
   {
     title: "www.aaroncurtisyoga.com",
-    url: "https://www.aaroncurtisyoga.com",
+    url: "/",
     desc: "",
+    internal: true,
   },
-  {
-    title: "ytt blog series",
-    url: "https://www.yogadistrict.com/category/training/",
-    desc: "",
-  },
+  // {
+  //   title: "ytt blog series",
+  //   url: "https://www.yogadistrict.com/category/training/",
+  //   desc: "",
+  //   internal: false,
+  // },
   {
     title: "playlists",
     url: "https://open.spotify.com/user/31fmmphtelatfs7ra4tvboorm4qy",
     desc: "",
+    internal: false,
   },
 ];
