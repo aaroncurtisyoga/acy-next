8daed522409d1f7a638ea374d81b0fbd27e949c7
aaroncurtisyoga <aaroncurtisyoga@gmail.com>
2023-10-03 19:32:12 -0400

    Placeholder for my schedule to be on homepage

---
+++ src/components/schedule/schedule.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import classes from "./schedule.module.scss";
+
+/* Todo:
+*   1) Mobile appearance
+*   2) Desktop Appearance
+* */
+
+const Schedule = () => {
+  return (
+    <section className={classes.scheduleSection}>
+      <p>my schedule here</p>
+    </section>
+  )
+}
+
+export default Schedule
