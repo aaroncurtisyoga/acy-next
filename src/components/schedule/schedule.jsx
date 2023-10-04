7d6d8c0b2de1ad4c5e1ba4c6191b95e2a4ff2c33
aaroncurtisyoga <aaroncurtisyoga@gmail.com>
2023-10-03 20:23:23 -0400

    Attempt to fix Build Error. And, add classes for Monday

--- src/components/schedule/schedule.jsx
+++ src/components/schedule/schedule.jsx
@@ -2,16 +2,32 @@ import React from "react";
 import classes from "./schedule.module.scss";
 
 /* Todo:
-*   1) Mobile appearance
-*   2) Desktop Appearance
-* */
+ *   1) Check w/ Google/Material and see if there's better way to display
+ *  list of classes
+ * */
 
 const Schedule = () => {
+  const dcBoulderingProject =
+    "https://www.dcboulderingproject.com/yoga-fitness";
+  const yogaDistrictLink = "https://www.bit.ly/yogadistrictaaron";
+
   return (
     <section className={classes.scheduleSection}>
-      <p>my schedule here</p>
+      <p>Mondays</p>
+      <li className={classes.yogaClass}>
+        6:30am - Power Flow - Bouldering Project DC
+        <a className={classes.signUpLink} href={dcBoulderingProject}>
+          <em>Sign Up</em>
+        </a>
+      </li>
+      <li className={classes.yogaClass}>
+        6:30pm - All Levels Flow - Yoga District{" "}
+        <a className={classes.signUpLink} href={yogaDistrictLink}>
+          <em>Sign Up</em>
+        </a>
+      </li>
     </section>
-  )
-}
+  );
+};
 
-export default Schedule
+export default Schedule;
