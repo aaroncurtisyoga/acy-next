88a6eb3514482aa3c4f80794d6c5a9dabe7ae285
aaroncurtisyoga <aaroncurtisyoga@gmail.com>
2023-10-04 19:18:26 -0400

    Complete schedule. Add welcome message.

--- src/components/landing-page/landing-page.jsx
+++ src/components/landing-page/landing-page.jsx
@@ -21,7 +21,18 @@ const LandingPage = () => {
       </div>
       <div className={classes.contentCopy}>
         <div className={classes.name}>Aaron Curtis</div>
+        <h1 className={classes.headline}>Flow.</h1>
+        <p className={classes.subHeadlineCopy}>
+          ✌🏾 What&apos;s up y&apos;all, I&apos;d love to practice with you! I
+          teach just about every day of the week. And, in multiple places
+          throughout Washington, D.C. So drop in and say hey 🙂 My classes are
+          inspired from various styles, typically include time for meditation
+          and breath work, and every once in a while, a sound bath for savasana.
+          I hope to see you on the mat soon!
+        </p>
+        <br />
         <Schedule />
+        <br />
         <IconLinks />
       </div>
     </section>
--- src/components/schedule/schedule.jsx
+++ src/components/schedule/schedule.jsx
@@ -1,31 +1,91 @@
 import React from "react";
 import classes from "./schedule.module.scss";
 
-/* Todo:
- *   1) Check w/ Google/Material and see if there's better way to display
- *  list of classes
- * */
-
 const Schedule = () => {
   const dcBoulderingProject =
     "https://www.dcboulderingproject.com/yoga-fitness";
   const yogaDistrictLink = "https://www.bit.ly/yogadistrictaaron";
+  const crossFitDcLink = "https://crossfitdc.com/";
 
   return (
     <section className={classes.scheduleSection}>
-      <p>Mondays</p>
-      <li className={classes.yogaClass}>
-        6:30am - Power Flow - Bouldering Project DC
-        <a className={classes.signUpLink} href={dcBoulderingProject}>
-          <em>Sign Up</em>
-        </a>
-      </li>
-      <li className={classes.yogaClass}>
-        6:30pm - All Levels Flow - Yoga District{" "}
-        <a className={classes.signUpLink} href={yogaDistrictLink}>
-          <em>Sign Up</em>
-        </a>
-      </li>
+      <div>
+        <p className={classes.day}>Monday</p>
+        <p>
+          6:30am - Power Flow - Bouldering Project DC{" "}
+          <a href={dcBoulderingProject} rel="noreferrer" target="_blank">
+            <em>Sign Up</em>
+          </a>
+        </p>
+        <p>
+          6:30pm - All Levels Flow - Yoga District{" "}
+          <a href={yogaDistrictLink} rel="noreferrer" target="_blank">
+            <em>Sign Up</em>
+          </a>
+        </p>
+      </div>
+      <br />
+
+      <div>
+        <p className={classes.day}>Tuesday</p>
+        <p>
+          6:45pm - Power Flow 1.5/3 - Yoga District{" "}
+          <a href={yogaDistrictLink} rel="noreferrer" target="_blank">
+            <em>Sign Up</em>
+          </a>
+        </p>
+      </div>
+      <br />
+
+      <div className={classes.day}>
+        <p className={classes.day}>Thursday</p>
+        <p>
+          12:00pm - Flow - Bouldering Project DC{" "}
+          <a href={dcBoulderingProject} rel="noreferrer" target="_blank">
+            <em>Sign Up</em>
+          </a>
+        </p>
+      </div>
+      <br />
+
+      <div>
+        <p className={classes.day}>Friday</p>
+        <p>
+          6:30am - Power Flow - Bouldering Project DC{" "}
+          <a href={dcBoulderingProject} rel="noreferrer" target="_blank">
+            <em>Sign Up</em>
+          </a>
+        </p>
+      </div>
+      <br />
+
+      <div>
+        <p className={classes.day}>Saturday</p>
+        <p>
+          10:30am - Flow 1.5/3 - Yoga District{" "}
+          <a href={yogaDistrictLink} rel="noreferrer" target="_blank">
+            <em>Sign Up</em>
+          </a>
+        </p>
+      </div>
+      <br />
+
+      <div className={classes.day}>
+        <p className={classes.day}>Sunday</p>
+        <p>
+          {" "}
+          10:45am - Flow 2/3 - Yoga District{" "}
+          <a href={yogaDistrictLink} rel="noreferrer" target="_blank">
+            <em>Sign Up</em>
+          </a>
+        </p>
+        <p>
+          2:00pm - Mobility/Flexibility - CrossFit DC{" "}
+          <a href={crossFitDcLink} rel="noreferrer" target="_blank">
+            <em>Sign Up</em>
+          </a>
+        </p>
+      </div>
     </section>
   );
 };
