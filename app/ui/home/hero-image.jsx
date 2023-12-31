a7b5512a645da301bf3c8d90bae5c11d3312b9f3
Aaron Curtis <77994989+aaroncurtisyoga@users.noreply.github.com>
2023-12-31 18:45:51 -0500

    Image priority

--- app/ui/home/hero-image.jsx
+++ app/ui/home/hero-image.jsx
@@ -7,6 +7,7 @@ const HeroImage = () => {
         alt="Yoga posture hand to big toe"
         className="object-cover"
         fill={true}
+        priority={true}
         loading="eager"
         src="/images/042321_YogaPose_HandToBigToe.jpg"
       />
