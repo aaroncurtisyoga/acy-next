7b4d947c75bfe0d4831b8c82e58af966bb74d7ae
Aaron Curtis <77994989+aaroncurtisyoga@users.noreply.github.com>
2023-12-31 18:46:20 -0500

    Start to server action

--- app/_actions.ts
+++ app/_actions.ts
@@ -1,17 +1,53 @@
 "use server";
 
+import mailchimp, { ErrorResponse } from "@mailchimp/mailchimp_marketing";
+
 import { z } from "zod";
 import { FormNewsletterSchema } from "@/lib/schema";
 
 type Inputs = z.infer<typeof FormNewsletterSchema>;
 
 export async function addEmailToNewsletter(data: Inputs) {
+  console.log("Inside the addEmailToNewsletter fn");
   const result = FormNewsletterSchema.safeParse(data);
+  // If server-side form validation successful, then Post to Mailchimp
   if (result.success) {
-    // todo: do some things here w/ mailchimp
-    return {
-      success: true,
-      data: result.data,
-    };
+    // Initialize the Mailchimp client
+    mailchimp.setConfig({
+      apiKey: process.env.MAILCHIMP_API_KEY,
+      server: process.env.MAILCHIMP_API_SERVER,
+    });
+
+    try {
+      console.log("inside try");
+      const addNew = await mailchimp.lists.addListMember(
+        String(process.env.MAILCHIMP_AUDIENCE_ID),
+        {
+          email_address: data.email,
+          status: "subscribed",
+        },
+      );
+      console.log("addNew is");
+      console.log(addNew);
+      return { message: "Successfully subscribed email." };
+    } catch (error: any) {
+      console.log("Inside catch");
+      if (
+        error.response?.status === 400 &&
+        error.response?.text?.includes("Member Exists")
+      ) {
+        console.log(error);
+        return { error: "ALREADY_SUBSCRIBED", message: "Already subscribed" };
+      }
+
+      console.error("Error subscribing email:", error);
+      return {
+        error: "Failed to subscribe.",
+        message: "Something went wrong, please try again later.",
+      };
+    }
+  } else {
+    // Todo: Do something if SS Form Validation doesn't work
+    console.log("Server: In the Else statement");
   }
 }
--- app/newsletter/page.tsx
+++ app/newsletter/page.tsx
@@ -24,7 +24,6 @@ const NewsletterForm = () => {
 
     if (!result) {
       console.log("!result");
-      console.log(result.error);
       return;
     }
 
@@ -39,11 +38,11 @@ const NewsletterForm = () => {
   };
 
   return (
-    <div className="bg-white dark:bg-gray-900 shadow-lg p-6 rounded-md gap-2 flex flex-col">
-      <h2 className="text-2xl font-bold text-gray-900 dark:text-gray-200 mb-4">
+    <div className="bg-white shadow-lg p-6 rounded-md gap-2 flex flex-col">
+      <h2 className="text-2xl font-bold text-gray-900 mb-4">
         Subscribe to my Newsletter
       </h2>
-      <p className="text-gray-600 dark:text-gray-400">
+      <p className="text-gray-90000">
         Be the first to know for upcoming workshops, classes, and more :)
       </p>
       {/* server action would usually use the 'action' attr, but w/ use-react-hook it reqs onSubmit*/}
@@ -51,13 +50,15 @@ const NewsletterForm = () => {
         <label>Email Address</label>
         <input
           aria-invalid={errors.email ? "true" : "false"}
-          className="border disabled:cursor-not-allowed disabled:opacity-50 border-gray-300 bg:gray-50 dark:bg-gray-800 dark:text-gray-300 dark:border-gray-700 rounded-md py-2 px-4 mb-4 focus:outline-none focus:ring-2 focus:ring-blue-500 dark:focus:ring-blue-300"
+          className="border disabled:cursor-not-allowed disabled:opacity-50 border-gray-300 bg:gray-50 rounded-md py-2 px-4 mb-4 focus:outline-none focus:ring-2 focus:ring-blue-500"
           {...register("email")}
         />
         <button
           type="submit"
-          className="bg-blue-500 disabled:cursor-not-allowed disabled:opacity-50 dark:bg-blue-800 text-white font-medium py-2 px-4 rounded-md hover:bg-blue-600 dark:hover:bg-blue-700 transition-colors duration-300"
-        ></button>
+          className="bg-blue-500 disabled:cursor-not-allowed disabled:opacity-50 text-white font-medium py-2 px-4 rounded-md hover:bg-blue-600 transition-colors duration-300"
+        >
+          Subscribe
+        </button>
         {/*{errors.email?.message && <p role="alert">{errors.email?.message}</p>}*/}
       </form>
     </div>
