24e074e1b58d5167f7b54ca5b8d2eeb2e842ef1e
Aaron Curtis <77994989+aaroncurtisyoga@users.noreply.github.com>
2024-01-12 16:31:11 -0500

    Speed insights + img blur

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,17 +2,19 @@ import "@/app/globals.css";
 import { Poppins } from "next/font/google";
 import Header from "@/components/shared/Header";
 import Image from "next/image";
-
+import handstandPicture from "../public/images/HandstandScissorLeg.png";
 const poppins = Poppins({
   subsets: ["latin"],
   weight: ["400", "500", "600", "700"],
   variable: "--font-poppins",
 });
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className={`${poppins.variable} antialiased flex flex-col h-dvh`}>
+        <SpeedInsights />
         <Header />
         <main
           className={
@@ -24,10 +26,11 @@ export default function Layout({ children }: { children: React.ReactNode }) {
               alt="Yoga posture hand to big toe"
               className="object-cover"
               fill={true}
-              sizes={"(min-width: 768px) 50vw, 0vw"}
+              sizes="(min-width: 1640px) 768px, calc(45vw + 39px)"
               priority={true}
+              placeholder={"blur"}
               loading="eager"
-              src="/images/ScissorHandstand_LowRes-min.jpg"
+              src={handstandPicture}
             />
           </section>
           {children}
