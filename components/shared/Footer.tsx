cbee54a2368b661f32bd2fc92ff42d4fdc5e1841
Aaron Curtis <77994989+aaroncurtisyoga@users.noreply.github.com>
2024-01-18 12:09:40 -0500

    Add todos

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,16 +1,23 @@
 import React from "react";
 import { AudioLines, Instagram, Youtube } from "lucide-react";
+
+/* Todo
+ *   Improve styles
+ *   Make icons actual links
+ * */
 const Footer = () => {
   return (
-    <footer className={"w-full"}>
+    <footer className={"w-full border-t"}>
       <div className="wrapper flex md:justify-between">
-        <div>© {new Date().getFullYear()} All rights reserved.</div>
+        <div className={"hidden md:block"}>
+          © {new Date().getFullYear()} All rights reserved.
+        </div>
         <div className={"flex justify-between"}>
           <AudioLines />
           <Instagram />
           <Youtube />
         </div>
-        <div>Made by Aaron</div>
+        <div className={"hidden md:block"}>Made by Aaron</div>
       </div>
     </footer>
   );
--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import Link from "next/link";
 import React from "react";
 import NavItems from "@/components/shared/NavItems";
