9802088e20928404bf7a6375336e358434a0c685
Aaron Curtis <77994989+aaroncurtisyoga@users.noreply.github.com>
2024-01-18 11:47:58 -0500

    Footer styles - start

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -2,14 +2,16 @@ import React from "react";
 import { AudioLines, Instagram, Youtube } from "lucide-react";
 const Footer = () => {
   return (
-    <footer className={"w-full flex md:justify-between"}>
-      <div>© {new Date().getFullYear()} All rights reserved.</div>
-      <div className={"flex justify-between"}>
-        <AudioLines />
-        <Instagram />
-        <Youtube />
+    <footer className={"w-full"}>
+      <div className="wrapper flex md:justify-between">
+        <div>© {new Date().getFullYear()} All rights reserved.</div>
+        <div className={"flex justify-between"}>
+          <AudioLines />
+          <Instagram />
+          <Youtube />
+        </div>
+        <div>Made by Aaron</div>
       </div>
-      <div>Made by Aaron</div>
     </footer>
   );
 };
