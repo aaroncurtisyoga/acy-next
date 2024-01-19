ef3a0c7b4ed81c800c7cb8d8f7712bff5a5fbb8c
Aaron Curtis <77994989+aaroncurtisyoga@users.noreply.github.com>
2024-01-19 13:37:07 -0500

    User schema

---
+++ lib/mongodb/database/models/user.model.ts
@@ -0,0 +1,23 @@
+import { model, models, Schema } from "mongoose";
+
+const UserSchema = new Schema({
+  clerkId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  // Todo: Might not need username. Consider removing at a later date.
+  username: { type: String, required: true, unique: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  photo: { type: String, required: true },
+});
+
+const User = models.User || model("User", UserSchema);
+
+export default User;
