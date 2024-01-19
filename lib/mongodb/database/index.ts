5a8ebb065c29c7b2201055a21d17c60fc09eb370
Aaron Curtis <77994989+aaroncurtisyoga@users.noreply.github.com>
2024-01-19 13:01:45 -0500

    Implement MongoDB connection caching

--- lib/mongodb/database/index.ts
+++ lib/mongodb/database/index.ts
@@ -1,8 +1,30 @@
 import mongoose from "mongoose";
+const MONGODB_URI = process.env.MONGODB_URI;
 
-let cached = mongoose || {
+// Hold a cached connection to the db
+let cached = (global as any).mongoose || {
   conn: null,
   promise: null,
 };
 
-export const connectToDatabase = async () => {};
+export const connectToDatabase = async () => {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!MONGODB_URI) {
+    throw new Error("MONGODB_URI is missing");
+  }
+
+  // Connect to already existing cached connection. Or, create a new connection
+  cached.promise =
+    cached.promise ||
+    mongoose.connect(MONGODB_URI, {
+      dbName: "ACY Events",
+      bufferCommands: false,
+    });
+
+  cached.conn = await cached.promise;
+
+  return cached.conn;
+};
