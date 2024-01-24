aed2df21d667aa4c45875ee87a18701d0b746020
Aaron Curtis <77994989+aaroncurtisyoga@users.noreply.github.com>
2024-01-24 17:21:49 -0500

    Start EventDetails page

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -1,5 +1,25 @@
-const EventDetails = () => {
-  return <div>event details</div>;
+import Image from "next/image";
+import { SearchParamProps } from "@/types";
+import { getEventById } from "@/lib/actions/event.actions";
+
+const EventDetails = async ({ params: { id } }: SearchParamProps) => {
+  const event = await getEventById(id);
+
+  return (
+    <>
+      <section className="flex justify-center bg-primary-50 bg-dotted-pattern bg-contain">
+        <div className="grid grid-cols-1 md:grid-cols-2 2xl:max-w-7xl">
+          <Image
+            src={event.imageUrl}
+            alt="hero image"
+            width={1000}
+            height={1000}
+            className="h-full min-h-[300px] object-cover object-center"
+          />
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default EventDetails;
--- lib/actions/event.actions.ts
+++ lib/actions/event.actions.ts
@@ -4,6 +4,7 @@ import { revalidatePath } from "next/cache";
 import { connectToDatabase } from "@/lib/mongodb/database";
 import { handleError } from "@/lib/utils";
 import Event from "@/lib/mongodb/database/models/event.model";
+import Category from "@/lib/mongodb/database/models/category.model";
 
 export async function createEvent({ event, path }) {
   try {
@@ -15,6 +16,26 @@ export async function createEvent({ event, path }) {
     });
     revalidatePath(path);
     return JSON.parse(JSON.stringify(newEvent));
+  } catch (error) {
+    handleError(error);
+  }
+}
+
+const populateEvent = (query: any) => {
+  return query.populate({
+    path: "category",
+    model: Category,
+    select: "_id name",
+  });
+};
+export async function getEventById(eventId: string) {
+  try {
+    await connectToDatabase();
+    const event = await populateEvent(Event.findById(eventId));
+
+    if (!event) throw new Error("Event not found");
+
+    return JSON.parse(JSON.stringify(event));
   } catch (error) {
     handleError(error);
   }
