645d2c54d4ca6a622f2b5434a791024ec813c73c
Aaron Curtis <77994989+aaroncurtisyoga@users.noreply.github.com>
2024-01-29 14:27:36 -0500

    Refactor EventForm onSubmit method

--- components/events/EventForm.tsx
+++ components/events/EventForm.tsx
@@ -46,6 +46,35 @@ const EventForm = ({ event, type }: EventFormProps) => {
     defaultValues: eventInitialValues,
   });
 
+  async function createNewEvent(values: z.infer<typeof EventFormSchema>) {
+    try {
+      const newEvent = await createEvent({ event: values, path: "/events" });
+
+      if (newEvent) {
+        form.reset();
+        router.push(`/events/${newEvent._id}`);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async function updateExistingEvent(values: z.infer<typeof EventFormSchema>) {
+    try {
+      const updatedEvent = await updateEvent({
+        event: { ...values, _id: event._id },
+        path: `/events/${event._id}`,
+      });
+
+      if (updatedEvent) {
+        form.reset();
+        router.push(`/events/${updatedEvent._id}`);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   async function onSubmit(values: z.infer<typeof EventFormSchema>) {
     if (!values.imageUrl) {
       form.setError("imageUrl", {
@@ -56,36 +85,14 @@ const EventForm = ({ event, type }: EventFormProps) => {
     }
 
     if (type === "Create") {
-      try {
-        const newEvent = await createEvent({ event: values, path: "/events" });
-
-        if (newEvent) {
-          form.reset();
-          router.push(`/events/${newEvent._id}`);
-        }
-      } catch (error) {
-        console.log(error);
-      }
+      await createNewEvent(values);
     }
 
     if (type === "Update") {
-      if (!event._id) {
+      if (event._id) {
+        await updateExistingEvent(values);
+      } else {
         router.back();
-        return;
-      }
-
-      try {
-        const updatedEvent = await updateEvent({
-          event: { ...values, _id: event._id },
-          path: `/events/${event._id}`,
-        });
-
-        if (updatedEvent) {
-          form.reset();
-          router.push(`/events/${updatedEvent._id}`);
-        }
-      } catch (error) {
-        console.log(error);
       }
     }
   }
