616bc417d2c79b8133e8ba65b21456203729c267
Aaron Curtis <77994989+aaroncurtisyoga@users.noreply.github.com>
2024-01-29 17:05:57 -0500

    Begin Profile page

---
+++ app/(root)/profile/page.tsx
@@ -0,0 +1,43 @@
+import Link from "next/link";
+import React from "react";
+import { auth } from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
+import Collection from "@/components/events/Collection";
+import { SearchParamProps } from "@/types";
+
+import { IOrder } from "@/lib/mongodb/database/models/order.model";
+import { getOrdersByUser } from "@/lib/actions/order.actions";
+
+const ProfilePage = async ({ searchParams }: SearchParamProps) => {
+  const { sessionClaims } = auth();
+  const userId = sessionClaims?.metadata?.userId as string;
+  const ordersPage = Number(searchParams?.ordersPage) || 1;
+  const orders = await getOrdersByUser({ userId, page: ordersPage });
+  const orderedEvents = orders?.data.map((order: IOrder) => order.event) || [];
+  return (
+    <>
+      <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
+        <div className="wrapper flex items-center justify-center sm:justify-between">
+          <h3 className="h3-bold text-center sm:text-left">My Tickets</h3>
+          <Button asChild size="lg" className="button hidden sm:flex">
+            <Link href="/#events">Explore More Events</Link>
+          </Button>
+        </div>
+      </section>
+      <section className="wrapper my-8">
+        <Collection
+          data={orderedEvents}
+          emptyTitle="No event tickets purchased yet"
+          emptyStateSubtext="No worries - plenty of exciting events to explore!"
+          collectionType="My_Tickets"
+          limit={3}
+          page={ordersPage}
+          urlParamName="ordersPage"
+          totalPages={orders?.totalPages}
+        />
+      </section>
+    </>
+  );
+};
+
+export default ProfilePage;
--- types/globals.d.ts
+++ types/globals.d.ts
@@ -7,6 +7,7 @@ declare global {
   interface CustomJwtSessionClaims {
     metadata: {
       role?: Roles;
+      userId: string;
     };
   }
 }
