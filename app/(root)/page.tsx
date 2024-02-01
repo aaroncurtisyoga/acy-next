9b3ad1d3d435e054beb84a4b1d9d9f1d505e6121
Aaron Curtis <77994989+aaroncurtisyoga@users.noreply.github.com>
2024-02-01 18:20:21 -0500

    Simplify homepage till I decide path forward

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,11 @@
 export default function Home() {
   return (
     <section className={"wrapper flex flex-col justify-center"}>
-      <div className={"text-4xl md:text-5xl lg:text-8xl"}>
-        <h1>Flow</h1>
-        <h1>Breathe</h1>
-        <h1>Meditate</h1>
+      <div>
+        <h1 className={"text-4xl text-center"}>
+          Yoga | Breathwork | Meditation
+        </h1>
       </div>
-      {/*<p className={"md:text-lg"}>Let&apos;s practice ✌🏾</p>*/}
     </section>
   );
 }
