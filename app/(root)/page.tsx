4313ff349a68269b39824052c4a5b2174f3274b6
Aaron Curtis <77994989+aaroncurtisyoga@users.noreply.github.com>
2024-02-04 17:10:21 -0500

    Make "Events" page.tsx the primary page for now

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,45 @@
-export default function Home() {
+import { Metadata } from "next";
+import Collection from "@/components/events/Collection";
+import { getAllEvents } from "@/lib/actions/event.actions";
+import { SearchParamProps } from "@/types";
+import Search from "@/components/shared/Search";
+import CategoryFilter from "@/components/events/CategoryFilter";
+
+export const metadata: Metadata = {
+  title: "Events",
+};
+const EventsPage = async ({ searchParams }: SearchParamProps) => {
+  const page = Number(searchParams?.page) || 1;
+  const searchText = (searchParams?.query as string) || "";
+  const category = (searchParams?.category as string) || "";
+
+  const events = await getAllEvents({
+    category,
+    limit: 8,
+    page,
+    query: searchText,
+  });
+
   return (
-    <section className={"wrapper flex flex-col justify-center"}>
-      <div>
-        <h1 className={"text-4xl text-center"}>
-          Yoga | Breathwork | Meditation
-        </h1>
-      </div>
-    </section>
+    <>
+      <section className={"wrapper flex flex-col gap-8 md:gap-12"}>
+        <h1 className={"text-2xl"}>Yoga | Breathwork | Meditation</h1>
+        <div className="flex w-full flex-col gap-5 md:flex-row">
+          <Search />
+          <CategoryFilter />
+        </div>
+        <Collection
+          data={events?.data}
+          emptyTitle={"No Events Founds"}
+          emptyStateSubtext={"Please visit back soon to check in for events."}
+          collectionType={"All_Events"}
+          limit={8}
+          page={page}
+          totalPages={events?.totalPages}
+        />
+      </section>
+    </>
   );
-}
+};
+
+export default EventsPage;
