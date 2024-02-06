a4e2ac4936073562cda5030316d013129196fc99
Aaron Curtis <77994989+aaroncurtisyoga@users.noreply.github.com>
2024-02-06 16:18:50 -0500

    2/1 aspect ratio for cards. Reduce roundedness

--- components/events/Card.tsx
+++ components/events/Card.tsx
@@ -33,8 +33,9 @@ const Card = ({ isAdmin, event }: CardProps) => {
       )}
       <div
         className={
-          "flex flex-col w-full max-w-[400px] min-h-[300px] overflow-hidden" +
-          " rounded-xl bg-white shadow-md transition-all hover:shadow-lg"
+          "flex flex-col w-full md:max-w-[400px]" +
+          " md:min-h-[300px] overflow-hidden" +
+          " rounded-sm bg-white shadow-md transition-all hover:shadow-lg"
         }
       >
         <Link href={`/events/${_id}`} className={"flex-col hover:underline"}>
@@ -44,13 +45,14 @@ const Card = ({ isAdmin, event }: CardProps) => {
             sizes={"400px"}
             width={400}
             height={140}
-            style={{ width: "100%", height: "140px", objectFit: "cover" }}
+            className={"aspect-[2/1] w-full h-auto md:h-[140px]"}
+            style={{ objectFit: "cover" }}
           />
           <p className="text-lg line-clamp-2 flex-1 text-black pt-2 px-3">
             {title}
           </p>
         </Link>
-        <div className={"pt-1 px-3"}>
+        <div className={"pt-1 px-3 mb-8 md:mb-auto"}>
           <p className="md:text-sm mb-1/2 font-semibold">
             {formatDateTime(startDateTime).dateOnlyWithoutYear} •{" "}
             {formatDateTime(startDateTime).timeOnly}
