a5a632435f0a05b4f60cc413e0d25d3e81be4de1
Aaron Curtis <77994989+aaroncurtisyoga@users.noreply.github.com>
2024-02-08 19:50:46 -0500

    limit to 50

--- app/api/get-blobs/route.ts
+++ app/api/get-blobs/route.ts
@@ -3,6 +3,6 @@ import { list } from "@vercel/blob";
 export const runtime = "edge";
 
 export async function GET(request: Request) {
-  const { blobs } = await list();
+  const { blobs } = await list({ limit: 50 });
   return Response.json(blobs);
 }
