e14abb2e0e2d77dde72da6eec0fd315335c286ca
Aaron Curtis <77994989+aaroncurtisyoga@users.noreply.github.com>
2024-02-22 16:31:10 -0500

    remove shadcn from NewsletterForm.tsx

--- components/newsletter/NewsletterForm.tsx
+++ components/newsletter/NewsletterForm.tsx
@@ -1,7 +1,6 @@
 "use client";
 
-import { useState } from "react";
-import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import { SubmitHandler, useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 
@@ -10,8 +9,13 @@ import { newsletterFormSchema } from "@/lib/schema";
 type Inputs = z.infer<typeof newsletterFormSchema>;
 
 const NewsletterForm = () => {
-  const [newsletterEntryAdded, setNewsletterEntryAdded] = useState(false);
-  const form = useForm<z.infer<typeof newsletterFormSchema>>({
+  const {
+    register,
+    handleSubmit,
+    setError,
+    reset,
+    formState: { errors, isSubmitting },
+  } = useForm<Inputs>({
     resolver: zodResolver(newsletterFormSchema),
     defaultValues: {
       first_name: "",
@@ -19,7 +23,7 @@ const NewsletterForm = () => {
     },
   });
 
-  const handleFormErrors = (
+  const handleErrorsFromServerSideValidation = (
     formErrors: z.ZodFormattedError<typeof newsletterFormSchema>,
   ) => {
     for (const formInput in formErrors) {
@@ -33,79 +37,39 @@ const NewsletterForm = () => {
     }
   };
 
-  const handleApiErrors = (result: { apiError?: string; message?: string }) => {
-    form.setError("root", {
+  const handleErrorsFromMailchimpApi = (result: {
+    apiError?: string;
+    message?: string;
+  }) => {
+    setError("root", {
       type: "server",
       message: result.message,
     });
   };
-
-  const onSubmit: SubmitHandler<Inputs> = async (data: FieldValues) => {
-    setNewsletterEntryAdded(false);
+  const onSubmit: SubmitHandler<Inputs> = async (data) => {
+    console.log("NewsletterForm onSubmit: triggered");
     const result = await addNewsletterEntry(data);
 
     if (result.formErrors) {
-      handleFormErrors(result.formErrors);
+      handleErrorsFromServerSideValidation(result.formErrors);
       return;
     }
 
     if (result.apiError) {
-      handleApiErrors(result);
+      handleErrorsFromMailchimpApi(result);
       return;
     }
 
-    setNewsletterEntryAdded(true);
-    form.reset();
+    console.log("NewsletterForm onSubmit: end of fn");
+    reset();
   };
 
   return (
-    <p>newsletter form</p>
-    /* <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)}>
-        <FormField
-          control={form.control}
-          name={"first_name"}
-          render={({ field }) => (
-            <FormItem>
-              {/!*<FormLabel>First Name</FormLabel>*!/}
-              <FormControl>
-                <Input {...field} placeholder={"First Name"} />
-              </FormControl>
-              <FormMessage className={"pl-3"} />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name={"email"}
-          render={({ field }) => (
-            <FormItem className={"mt-3"}>
-              {/!*<FormLabel>Email</FormLabel>*!/}
-              <FormControl>
-                <Input {...field} type="email" placeholder={"Email Address"} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-
-        <button type={"submit"} disabled={form.formState.isSubmitting}>
-          {form.formState.isSubmitting ? <p>Loading...</p> : "Subscribe"}
-        </button>
-        {form.formState.errors?.root && (
-          <>
-            <p>an error occurred</p>
-            <p>{form.formState.errors.root.message}</p>
-          </>
-        )}
-        {newsletterEntryAdded && (
-          <>
-            <p>All Set!</p>
-            <p>A confirmation email should be in your inbox soon.</p>
-          </>
-        )}
-      </form>
-    </Form>*/
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input defaultValue="First name" {...register("first_name")} />
+      <input defaultValue="Email address" {...register("email")} />
+      <input type="submit" disabled={isSubmitting} />
+    </form>
   );
 };
 
