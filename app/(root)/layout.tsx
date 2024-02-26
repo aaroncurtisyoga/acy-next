a35d6d4d2d35eee2dc810032ec43b9104b3b0c0c
Aaron Curtis <77994989+aaroncurtisyoga@users.noreply.github.com>
2024-02-26 14:02:48 -0500

    Comment-in Footer.tsx

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,9 +1,9 @@
-import { ReactNode } from "react";
-import Footer from "@/components/shared/Footer";
-import "@/app/globals.css";
 import Image from "next/image";
-import handstandPicture from "@/public/assets/images/ScissorHandstand_LowRes-min.jpg";
+import { ReactNode } from "react";
 import Header from "@/components/shared/Header";
+import Footer from "@/components/shared/Footer";
+import handstandPicture from "@/public/assets/images/ScissorHandstand_LowRes-min.jpg";
+import "@/app/globals.css";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
@@ -24,7 +24,7 @@ export default function Layout({ children }: { children: ReactNode }) {
           {children}
         </div>
       </main>
-      {/*<Footer />*/}
+      <Footer />
     </div>
   );
 }
