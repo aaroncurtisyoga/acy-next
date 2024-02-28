8ab18cb8eb2281532ad725e021ef9aa201c32521
Aaron Curtis <77994989+aaroncurtisyoga@users.noreply.github.com>
2024-02-28 10:50:58 -0500

    Begin styling EventCardAdminButtons.tsx

--- components/events/EventCardAdminButtons.tsx
+++ components/events/EventCardAdminButtons.tsx
@@ -1,5 +1,15 @@
 import Link from "next/link";
-import { Edit } from "lucide-react";
+import {
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Button,
+  useDisclosure,
+} from "@nextui-org/react";
+import { Edit, Trash } from "lucide-react";
+
 import { deleteEvent } from "@/lib/actions/event.actions";
 
 type EventCardAdminButtonsProps = {
@@ -11,25 +21,51 @@ const EventCardAdminButtons = ({
   id,
   pathname,
 }: EventCardAdminButtonsProps) => {
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+
   return (
-    <div className="flex flex-row gap-4 p-3 justify-between">
-      <Link href={`/orders?eventId=${id}`} className="flex gap-2">
-        <p className="text-primary-500">Order Details</p>
-      </Link>
-      <div className={"flex gap-2"}>
-        <Link href={`/events/${id}/update`}>
-          <Edit width={20} height={20} />
+    <>
+      <div className="flex flex-row gap-4 p-3 justify-between">
+        <Link href={`/orders?eventId=${id}`} className="flex gap-2">
+          <p className="text-primary-500">Order Details</p>
         </Link>
-        {/* todo: display confirmation msg in modal */}
-        <button
-          onClick={async () =>
-            await deleteEvent({ eventId: id, path: pathname })
-          }
-        >
-          delete event
-        </button>
+        <div className={"flex gap-2"}>
+          <Link href={`/events/${id}/update`}>
+            <Edit width={20} height={20} />
+          </Link>
+          <Button onPress={onOpen}>
+            <Trash width={20} height={20} />
+          </Button>
+        </div>
       </div>
-    </div>
+      <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader className="flex flex-col gap-1">
+                Confirm Delete
+              </ModalHeader>
+              <ModalBody>
+                <p>Are you sure you want to delete this event?</p>
+              </ModalBody>
+              <ModalFooter>
+                <Button color="primary" variant="light" onPress={onClose}>
+                  Close
+                </Button>
+                <Button
+                  color="danger"
+                  onPress={async () => {
+                    await deleteEvent({ eventId: id, path: pathname });
+                  }}
+                >
+                  Delete Event
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
+    </>
   );
 };
 
