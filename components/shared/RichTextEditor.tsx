85a73de9aa52bc447489e3cc3a0d8aaea61df3ae
Aaron Curtis <77994989+aaroncurtisyoga@users.noreply.github.com>
2024-03-09 11:45:52 -0500

    Add buttons to Editor. Pending: Indent/Outdent

--- components/shared/RichTextEditor.tsx
+++ components/shared/RichTextEditor.tsx
@@ -1,18 +1,79 @@
 "use client";
 
 import { useEditor, EditorContent } from "@tiptap/react";
-import StarterKit from "@tiptap/starter-kit";
 import Highlight from "@tiptap/extension-text";
-import { Paragraph } from "@tiptap/extension-paragraph";
+import StarterKit from "@tiptap/starter-kit";
+import TextAlign from "@tiptap/extension-text-align";
 import { BulletList } from "@tiptap/extension-bullet-list";
+import { Extension } from "@tiptap/core";
 import { OrderedList } from "@tiptap/extension-ordered-list";
+import { Paragraph } from "@tiptap/extension-paragraph";
 
-import { Bold, Italic } from "lucide-react";
+import {
+  AlignCenter,
+  AlignLeft,
+  AlignRight,
+  Bold,
+  IndentDecrease,
+  IndentIncrease,
+  Italic,
+  List,
+  ListOrdered,
+} from "lucide-react";
+
+const Indent = Extension.create({
+  name: "indent",
+
+  addCommands() {
+    return {
+      indent: ({ chain }) => {
+        return chain()
+          .focus()
+          .command(({ tr, selection }) => {
+            tr.forEachNodeAt(selection.getRanges(), (node, pos) => {
+              if (node.type.name === "paragraph") {
+                tr.setNodeMarkup(pos, undefined, {
+                  ...node.attrs,
+                  style: "padding-left: 40px;",
+                });
+              }
+            });
+            return true;
+          })
+          .run();
+      },
+      outdent: ({ chain }) => {
+        return chain()
+          .focus()
+          .command(({ tr, selection }) => {
+            tr.forEachNodeAt(selection.getRanges(), (node, pos) => {
+              if (node.type.name === "paragraph") {
+                tr.setNodeMarkup(pos, undefined, {
+                  ...node.attrs,
+                  style: "padding-left: 0px;",
+                });
+              }
+            });
+            return true;
+          })
+          .run();
+      },
+    };
+  },
+});
 
 const RichTextEditor = () => {
   const editor = useEditor({
-    extensions: [StarterKit, Highlight, Paragraph, BulletList, OrderedList],
-    content: "<p>Hello World! 🌎️</p>",
+    extensions: [
+      BulletList,
+      Highlight,
+      Indent,
+      OrderedList,
+      Paragraph,
+      StarterKit,
+      TextAlign,
+    ],
+    content: "",
   });
 
   if (!editor) {
@@ -34,6 +95,45 @@ const RichTextEditor = () => {
         >
           <Italic />
         </button>
+        <button
+          onClick={() => editor.chain().focus().setTextAlign("left").run()}
+        >
+          <AlignLeft />
+        </button>
+        <button
+          onClick={() => editor.chain().focus().setTextAlign("center").run()}
+        >
+          <AlignCenter />
+        </button>
+        <button
+          onClick={() => editor.chain().focus().setTextAlign("right").run()}
+        >
+          <AlignRight />
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleBulletList().run()}
+          className={editor.isActive("bulletList") ? "is-active" : ""}
+        >
+          <List />
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleOrderedList().run()}
+          className={editor.isActive("orderedList") ? "is-active" : ""}
+        >
+          <ListOrdered />
+        </button>
+        <button
+          onClick={() => editor.chain().focus().indent().run()}
+          className={editor.isActive("indent") ? "is-active" : ""}
+        >
+          <IndentIncrease />
+        </button>
+        <button
+          onClick={() => editor.chain().focus().outdent().run()}
+          className={editor.isActive("outdent") ? "is-active" : ""}
+        >
+          <IndentDecrease />
+        </button>
       </div>
       <EditorContent editor={editor} />
     </div>
@@ -41,47 +141,3 @@ const RichTextEditor = () => {
 };
 
 export default RichTextEditor;
-
-{
-  /*<button
-          onClick={() => editor.chain().focus().toggleBulletList().run()}
-          className={editor.isActive("bulletList") ? "is-active" : ""}
-        >
-          Bullet List
-        </button>
-        <button
-          onClick={() => editor.chain().focus().toggleOrderedList().run()}
-          className={editor.isActive("orderedList") ? "is-active" : ""}
-        >
-          Numbered List
-        </button>*/
-}
-{
-  /*<button
-          onClick={() => editor.chain().focus().indent().run()}
-          className={editor.isActive("indent") ? "is-active" : ""}
-        >
-          Indentation Right
-        </button>
-        <button
-          onClick={() => editor.chain().focus().outdent().run()}
-          className={editor.isActive("outdent") ? "is-active" : ""}
-        >
-          Indentation Left
-        </button>
-        <button
-          onClick={() => editor.chain().focus().setTextAlign("left").run()}
-        >
-          Text align left
-        </button>
-        <button
-          onClick={() => editor.chain().focus().setTextAlign("center").run()}
-        >
-          Text align center
-        </button>
-        <button
-          onClick={() => editor.chain().focus().setTextAlign("right").run()}
-        >
-          Text align right
-        </button>*/
-}
