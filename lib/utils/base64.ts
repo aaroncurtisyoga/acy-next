c29210e20bb80aec430029814300a357ec39ab5e
Aaron Curtis <77994989+aaroncurtisyoga@users.noreply.github.com>
2024-03-12 14:01:32 -0400

    Remove console log

--- lib/utils/base64.ts
+++ lib/utils/base64.ts
@@ -9,7 +9,6 @@ export async function getBase64(imageUrl: string) {
     }
     const buffer = await res.arrayBuffer();
     const { base64 } = await getPlaiceholder(Buffer.from(buffer));
-    console.log(`base64`, base64);
     return base64;
   } catch (error) {
     handleError(error);
