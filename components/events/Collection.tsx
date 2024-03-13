e824ffedaeb97f27ab1ca871cc45fe42e7617a11
Aaron Curtis <77994989+aaroncurtisyoga@users.noreply.github.com>
2024-03-13 17:15:14 -0400

    Styles for links

--- components/events/Collection.tsx
+++ components/events/Collection.tsx
@@ -3,7 +3,7 @@ import Pagination from "@/components/events/Pagination";
 import { IEvent } from "@/lib/mongodb/database/models/event.model";
 import { checkRole } from "@/lib/utils";
 
-type CollectionProps = {
+interface CollectionProps {
   data: IEvent[];
   emptyTitle: string;
   emptyStateSubtext: string;
@@ -12,7 +12,7 @@ type CollectionProps = {
   totalPages?: number;
   urlParamName?: string;
   collectionType?: "Events_Organized" | "My_Tickets" | "All_Events";
-};
+}
 
 const Collection = ({
   data,
--- components/events/EventPage/DirectionLinks.tsx
+++ components/events/EventPage/DirectionLinks.tsx
@@ -4,14 +4,17 @@ import { generateDirectionsUrl } from "@/lib/utils/travelLinks";
 interface DirectionLinksProps {
   location: { geometry: { lat: any; lng: any } };
 }
+
 const DirectionLinks = ({ location }: DirectionLinksProps) => {
   return (
-    <div className={"w-full"}>
-      <p className={"text-center"}>How to get there</p>
+    <div className={"w-full py-unit-4 border-b"}>
+      <p className={"text-center font-semibold text-default-700 mb-unit-6"}>
+        How to get there
+      </p>
       <div className=" text-primary-600">
         <ul className={"flex justify-center"}>
           {travelOptions.map((option) => (
-            <li key={option.travelMode}>
+            <li key={option.travelMode} className={"border-r last:border-r-0"}>
               <a
                 className={"px-unit-8 inline-block"}
                 target="_blank"
