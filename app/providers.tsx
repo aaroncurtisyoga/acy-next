9b4e926a829f4515bd8f0a40c5378a19f5471f20
Aaron Curtis <77994989+aaroncurtisyoga@users.noreply.github.com>
2024-03-16 15:41:22 -0400

    Connect NextUi Link to Next.js Link

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,12 +1,15 @@
 "use client";
 
+import { useRouter } from "next/navigation";
 import { NextUIProvider } from "@nextui-org/react";
 import { ClerkProvider } from "@clerk/nextjs";
 
 export function Providers({ children }: { children: React.ReactNode }) {
+  const router = useRouter();
+
   return (
     <ClerkProvider>
-      <NextUIProvider>{children}</NextUIProvider>
+      <NextUIProvider navigate={router.push}>{children}</NextUIProvider>
     </ClerkProvider>
   );
 }
