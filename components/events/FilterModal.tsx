1c1d4907019413c8953d6e23c746323e066f386c
Aaron Curtis <77994989+aaroncurtisyoga@users.noreply.github.com>
2024-03-17 19:34:22 -0400

    Convert to client component

--- components/events/FilterModal.tsx
+++ components/events/FilterModal.tsx
@@ -1,16 +1,46 @@
-import { Button } from "@nextui-org/react";
+"use client";
+
+import {
+  Button,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalHeader,
+  useDisclosure,
+} from "@nextui-org/react";
+
 import { SlidersHorizontal } from "lucide-react";
+import CategoryFilter from "@/components/events/CategoryFilter";
 
 const FilterModal = () => {
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+
   return (
     <>
       <Button
-        variant="light"
         color="default"
         endContent={<SlidersHorizontal />}
+        onPress={onOpen}
+        radius={"full"}
+        type={"button"}
+        variant="light"
       >
         Filters
       </Button>
+      <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
+        <ModalContent>
+          {() => (
+            <>
+              <ModalHeader className="flex flex-col gap-1 text-xl">
+                Search Filters
+              </ModalHeader>
+              <ModalBody>
+                <CategoryFilter />
+              </ModalBody>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
     </>
   );
 };
