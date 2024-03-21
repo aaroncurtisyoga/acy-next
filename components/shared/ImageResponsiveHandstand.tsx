a45e56bef1df9f2436560471ef6a1e50a47b0383
Aaron Curtis <77994989+aaroncurtisyoga@users.noreply.github.com>
2024-03-21 16:31:00 -0400

    Manually create blur data urls & host hero images in BLob

--- components/shared/ImageResponsiveHandstand.tsx
+++ components/shared/ImageResponsiveHandstand.tsx
@@ -1,8 +1,14 @@
 import { getImageProps } from "next/image";
-import handstandMobile from "@/public/assets/images/handstand_mobile.jpeg";
-import handstandDesktop from "@/public/assets/images/handstand_desktop.jpeg";
+import { getBase64 } from "@/lib/utils/base64";
 
 export default async function ImageResponsiveHandstand() {
+  const blurDataURLMobile = await getBase64(
+    "https://hyhtjblnarllhg0d.public.blob.vercel-storage.com/handstand_mobile-culATacmiJLfv88adfOLtp3q1uYeYH.jpeg",
+  );
+  const blurDataURLDesktop = await getBase64(
+    "https://hyhtjblnarllhg0d.public.blob.vercel-storage.com/handstand_desktop-o4lcMigM1mvB2fLffSaWmOS4BN2hqZ.jpeg",
+  );
+
   const common = {
     alt: "Aaron doing handstand posture",
     className: "object-cover",
@@ -13,26 +19,26 @@ export default async function ImageResponsiveHandstand() {
     props: { srcSet: mobile },
   } = getImageProps({
     ...common,
-    // blurDataURL: blurDataURLMobile,
+    blurDataURL: blurDataURLMobile,
     loading: "eager",
     placeholder: "blur",
     priority: true,
     quality: 70,
     sizes: "100vw",
-    src: handstandMobile,
+    src: "https://hyhtjblnarllhg0d.public.blob.vercel-storage.com/handstand_mobile-culATacmiJLfv88adfOLtp3q1uYeYH.jpeg",
   });
 
   const {
     props: { srcSet: desktop, ...rest },
   } = getImageProps({
     ...common,
-    // blurDataURL: blurDataURLDesktop,
+    blurDataURL: blurDataURLDesktop,
     loading: "eager",
     placeholder: "blur",
     priority: true,
     quality: 80,
     sizes: "50vw",
-    src: handstandDesktop,
+    src: "https://hyhtjblnarllhg0d.public.blob.vercel-storage.com/handstand_desktop-o4lcMigM1mvB2fLffSaWmOS4BN2hqZ.jpeg",
   });
 
   return (
