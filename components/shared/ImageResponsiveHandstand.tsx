6b4cbdbfa1a20b91e03c1cdc3b6f4fcdf4802052
Aaron Curtis <77994989+aaroncurtisyoga@users.noreply.github.com>
2024-03-21 16:20:28 -0400

    Try jpeg for getImageProps

--- components/shared/ImageResponsiveHandstand.tsx
+++ components/shared/ImageResponsiveHandstand.tsx
@@ -1,6 +1,8 @@
 import { getImageProps } from "next/image";
+import handstandMobile from "@/public/assets/images/handstand_mobile.jpeg";
+import handstandDesktop from "@/public/assets/images/handstand_desktop.jpeg";
 
-export default function ImageResponsiveHandstand() {
+export default async function ImageResponsiveHandstand() {
   const common = {
     alt: "Aaron doing handstand posture",
     className: "object-cover",
@@ -11,24 +13,26 @@ export default function ImageResponsiveHandstand() {
     props: { srcSet: mobile },
   } = getImageProps({
     ...common,
+    // blurDataURL: blurDataURLMobile,
     loading: "eager",
     placeholder: "blur",
     priority: true,
     quality: 70,
     sizes: "100vw",
-    src: "/assets/images/handstand_mobile.jpg",
+    src: handstandMobile,
   });
 
   const {
     props: { srcSet: desktop, ...rest },
   } = getImageProps({
     ...common,
+    // blurDataURL: blurDataURLDesktop,
     loading: "eager",
     placeholder: "blur",
     priority: true,
     quality: 80,
     sizes: "50vw",
-    src: "/assets/images/handstand_desktop.jpg",
+    src: handstandDesktop,
   });
 
   return (
