e1890ce449f3552f621c842abe28139e084e8d34
Aaron Curtis <77994989+aaroncurtisyoga@users.noreply.github.com>
2024-03-23 17:40:24 -0400

    Rmv collection from Account page

--- app/(root)/account/page.tsx
+++ app/(root)/account/page.tsx
@@ -1,22 +1,12 @@
 import Link from "next/link";
 import React from "react";
-import { auth } from "@clerk/nextjs";
 
-import Collection from "@/components/events/Collection";
-import { IOrder } from "@/lib/mongodb/database/models/order.model";
-import { getOrdersByUser } from "@/lib/actions/order.actions";
-import { SearchParamProps } from "@/types";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Profile",
 };
-const ProfilePage = async ({ searchParams }: SearchParamProps) => {
-  const { sessionClaims } = auth();
-  const userId = sessionClaims?.metadata?.userId as string;
-  const ordersPage = Number(searchParams?.ordersPage) || 1;
-  const orders = await getOrdersByUser({ userId, page: ordersPage });
-  const orderedEvents = orders?.data.map((order: IOrder) => order.event) || [];
+const ProfilePage = async () => {
   return (
     <>
       <section>
@@ -24,19 +14,6 @@ const ProfilePage = async ({ searchParams }: SearchParamProps) => {
           <h3>My Tickets</h3>
           <Link href="/#events">Explore More Events</Link>
         </div>
-      </section>
-      <section>
-        {/* Display in table instead... */}
-        <Collection
-          data={orderedEvents}
-          emptyTitle="No event tickets purchased yet"
-          emptyStateSubtext="Take a look at upcoming events to explore!"
-          collectionType="My_Tickets"
-          limit={3}
-          page={ordersPage}
-          urlParamName="ordersPage"
-          totalPages={orders?.totalPages}
-        />
       </section>
     </>
   );
