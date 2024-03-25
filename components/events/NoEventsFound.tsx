43e8f900cb90cead9c3aae85bc0361f6de77c7a3
Aaron Curtis <77994989+aaroncurtisyoga@users.noreply.github.com>
2024-03-25 13:55:14 -0400

    Adjust styles for NoEventsFound.tsx

--- components/events/NoEventsFound.tsx
+++ components/events/NoEventsFound.tsx
@@ -5,11 +5,13 @@ interface NoEventsFoundProps {
 const NoEventsFound = ({ hasFiltersApplied }: NoEventsFoundProps) => {
   return (
     <div className={"flex flex-col"}>
-      <p className={"font-bold text-xl md:text-2xl my-unit-3"}>
+      <p className={"font-bold text-xl md:text-2xl mt-unit-3 mb-unit-1"}>
         No Events Found
       </p>
       {hasFiltersApplied ? (
-        <p>Whoops... there&apos;`s no events that match your search</p>
+        <p className={"text-default-600"}>
+          Whoops... there&apos;s no events that match your search.
+        </p>
       ) : (
         <p className={"text-default-600"}>
           Whoops... there aren&apos;t any events listed just yet. Check back
