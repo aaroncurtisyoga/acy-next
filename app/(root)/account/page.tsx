7f4575202bccc108582db4d324c3737f7b87133c
Aaron Curtis <77994989+aaroncurtisyoga@users.noreply.github.com>
2024-03-25 15:37:48 -0400

    Wrapper for account page

--- app/(root)/account/page.tsx
+++ app/(root)/account/page.tsx
@@ -9,7 +9,12 @@ export const metadata: Metadata = {
 const ProfilePage = async () => {
   return (
     <>
-      <section>
+      <section
+        className={
+          "grid grow w-full md:min-h-[calc(100dvh-64px)] max-w-screen-2xl " +
+          "md:grid-cols-[1fr,1fr] 11rem)] lg:mx-auto"
+        }
+      >
         <div>
           <h3>My Tickets</h3>
           <Link href="/#events">Explore More Events</Link>
