8172711fd54dd7d3dd036074eb6787e60925598c
Aaron Curtis <77994989+aaroncurtisyoga@users.noreply.github.com>
2024-03-28 09:46:51 -0400

    Add log

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -12,8 +12,10 @@ import { NextResponse } from "next/server";
 export async function POST(req: Request) {
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhooks
   const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
+  console.log("WEBHOOK_SECRET", WEBHOOK_SECRET);
 
   if (!WEBHOOK_SECRET) {
+    console.log("No WEBHOOK_SECRET found in .env or .env.local");
     throw new Error(
       "Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local",
     );
