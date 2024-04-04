4497c23b459f63ada90ed3fe4ccf3a20a8ccebea
Aaron Curtis <77994989+aaroncurtisyoga@users.noreply.github.com>
2024-04-04 16:17:30 -0400

    Don't use Edge run time

--- app/api/get-blobs/route.ts
+++ app/api/get-blobs/route.ts
@@ -1,7 +1,5 @@
 import { list } from "@vercel/blob";
 
-export const runtime = "edge";
-
 export async function GET(request: Request, options) {
   const { blobs } = await list(options);
   return Response.json(blobs);
