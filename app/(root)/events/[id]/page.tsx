1c22fc09eec67f9c450030fcfe222f403e850db9
Aaron Curtis <77994989+aaroncurtisyoga@users.noreply.github.com>
2024-04-08 11:06:32 -0400

    Clean up event page 2

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -27,44 +27,38 @@ const EventPage = async ({ params: { id } }: SearchParamProps) => {
   } = event;
 
   return (
-    <>
-      <section className="flex flex-col w-full md:items-center pb-unit-10 gap-3">
-        <Hero imageUrl={imageUrl} />
-        <Subheading
-          category={category}
-          id={_id}
-          startDateTime={startDateTime}
-        />
-        <div className={"wrapper-width flex flex-col md:flex-row"}>
-          <div className={"flex-1"}>
-            <div className={"px-5 md:px-0"}>
-              <Headline title={title} />
-              <DateAndTime
-                startDateTime={startDateTime}
-                endDateTime={endDateTime}
-              />
-              <Location location={location} />
-              <DescriptionRichTextEditor description={description} />
-              <Attendees attendees={attendees} />
-              {!isFree && <RefundPolicy />}
-            </div>
-          </div>
-          <div
-            id={"event-checkout"}
-            className={
-              "flex-1 w-full border-t-2 h-[140px] p-[24px] fixed bottom-0 z-10 bg-white" +
-              " md:border-[1px] md:rounded-2xl md:relative" +
-              " md:max-w-[360px]"
-            }
-          >
-            <p className={"text-center text-lg mb-3"}>
-              {isFree ? "Free" : `$${price}`}
-            </p>
-            <CheckoutButton event={event} className={"justify-center"} />
+    <section className="flex flex-col w-full md:items-center pb-unit-10 gap-3">
+      <Hero imageUrl={imageUrl} />
+      <Subheading category={category} id={_id} startDateTime={startDateTime} />
+      <div className={"wrapper-width flex flex-col md:flex-row"}>
+        <div className={"flex-1"}>
+          <div className={"px-5 md:px-0"}>
+            <Headline title={title} />
+            <DateAndTime
+              startDateTime={startDateTime}
+              endDateTime={endDateTime}
+            />
+            <Location location={location} />
+            <DescriptionRichTextEditor description={description} />
+            <Attendees attendees={attendees} />
+            {!isFree && <RefundPolicy />}
           </div>
         </div>
-      </section>
-    </>
+        <div
+          id={"event-checkout"}
+          className={
+            "flex-1 w-full border-t-2 h-[140px] p-[24px] fixed bottom-0 z-10 bg-white" +
+            " md:border-[1px] md:rounded-2xl md:relative" +
+            " md:max-w-[360px]"
+          }
+        >
+          <p className={"text-center text-lg mb-3"}>
+            {isFree ? "Free" : `$${price}`}
+          </p>
+          <CheckoutButton event={event} className={"justify-center"} />
+        </div>
+      </div>
+    </section>
   );
 };
 
