ad4278a551e7778069410e7c65b3275f1429c309
Aaron Curtis <77994989+aaroncurtisyoga@users.noreply.github.com>
2024-04-08 11:04:53 -0400

    Clean up event page

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -1,30 +1,31 @@
+import Attendees from "@/components/events/EventPage/Attendees";
 import CheckoutButton from "@/components/events/EventPage/CheckoutButton";
 import DateAndTime from "@/components/events/EventPage/DateAndTime";
+import DescriptionRichTextEditor from "@/components/events/EventPage/DescriptionDisplay";
+import Headline from "@/components/events/EventPage/Headline";
 import Hero from "@/components/events/EventPage/Hero";
 import Location from "@/components/events/EventPage/Location";
-import Subheading from "@/components/events/EventPage/Subheadline";
-import Headline from "@/components/events/EventPage/Headline";
 import RefundPolicy from "@/components/events/EventPage/RefundPolicy";
-import {
-  getEventById,
-  getEventsWithSameCategory,
-} from "@/lib/actions/event.actions";
+import Subheading from "@/components/events/EventPage/Subheadline";
+import { getEventById } from "@/lib/actions/event.actions";
 import { SearchParamProps } from "@/types";
-import DescriptionRichTextEditor from "@/components/events/EventPage/DescriptionDisplay";
-import Attendees from "@/components/events/EventPage/Attendees";
-import attendees from "@/components/events/EventPage/Attendees";
 
-const EventPage = async ({
-  params: { id },
-  searchParams,
-}: SearchParamProps) => {
+const EventPage = async ({ params: { id } }: SearchParamProps) => {
   const event = await getEventById(id);
-  const { _id, category, imageUrl, startDateTime, endDateTime, title } = event;
-  const eventsWithSameCategory = await getEventsWithSameCategory({
-    categoryId: category._id,
-    eventId: _id,
-    page: searchParams.page as string,
-  });
+  const {
+    _id,
+    attendees,
+    category,
+    description,
+    endDateTime,
+    imageUrl,
+    isFree,
+    location,
+    price,
+    startDateTime,
+    title,
+  } = event;
+
   return (
     <>
       <section className="flex flex-col w-full md:items-center pb-unit-10 gap-3">
@@ -42,10 +43,10 @@ const EventPage = async ({
                 startDateTime={startDateTime}
                 endDateTime={endDateTime}
               />
-              <Location location={event.location} />
-              <DescriptionRichTextEditor description={event.description} />
-              <Attendees attendees={event.attendees} />
-              {!event.isFree && <RefundPolicy />}
+              <Location location={location} />
+              <DescriptionRichTextEditor description={description} />
+              <Attendees attendees={attendees} />
+              {!isFree && <RefundPolicy />}
             </div>
           </div>
           <div
@@ -57,32 +58,12 @@ const EventPage = async ({
             }
           >
             <p className={"text-center text-lg mb-3"}>
-              {event.isFree ? "Free" : `$${event.price}`}
+              {isFree ? "Free" : `$${price}`}
             </p>
             <CheckoutButton event={event} className={"justify-center"} />
           </div>
         </div>
       </section>
-      {/* {!!eventsWithSameCategory?.data.length && (
-        <section className={"bg-gray-100 w-full py-6 md:py-8"}>
-          <div className="event-wrapper-width">
-            <h3 className={"text-lg font-bold text-gray-800 mb-6"}>
-              Other events with Aaron you may like:
-            </h3>
-            <Collection
-              data={eventsWithSameCategory?.data}
-              emptyTitle={"No Events Founds"}
-              emptyStateSubtext={
-                "Please visit back soon to check in for events."
-              }
-              collectionType={"All_Events"}
-              limit={3}
-              page={searchParams.page as string}
-              totalPages={eventsWithSameCategory?.totalPages}
-            />
-          </div>
-        </section>
-      )}*/}
     </>
   );
 };
