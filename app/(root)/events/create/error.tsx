5931a9376b3ebc597177e0dcf99ac15290e24ecc
Aaron Curtis <77994989+aaroncurtisyoga@users.noreply.github.com>
2024-04-10 14:56:00 -0400

    Err pg for creating event

---
+++ app/(root)/events/create/error.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import Error from "@/components/shared/Error";
+
+export default function ErrorBoundary() {
+  return (
+    <Error>
+      <h1 className={"error-headline"}>
+        Whoops, we are having trouble creating your event. Please try again.
+      </h1>
+    </Error>
+  );
+}
