2835fad85f740d717de92a4a6dacdd47c1c0e1b3
Aaron Curtis <77994989+aaroncurtisyoga@users.noreply.github.com>
2024-04-10 14:28:08 -0400

    Finish Err styles

--- app/(root)/events/[id]/error.tsx
+++ app/(root)/events/[id]/error.tsx
@@ -6,18 +6,18 @@ import { Link as NextUiLink } from "@nextui-org/link";
 
 export default function ErrorBoundary() {
   return (
-    <div className="flex flex-col justify-center items-center max-w-4xl mx-auto mt-16 gap-4">
+    <div className="flex flex-col justify-center items-center max-w-4xl mx-auto my-16 gap-4 text-center px-3">
       <CircleAlert className={"text-yellow-500"} size={100} />
-      <h1 className={"text-5xl text-center font-extrabold "}>
+      <h1 className={"text-3xl md:text-5xl  font-extrabold "}>
         Whoops, the page or event you are looking for was not found.
       </h1>
-      <h2 className={"text-2xl text-gray-500"}>
+      <h2 className={"text-xl md:text-2xl text-gray-500"}>
         If you feel this message is in error, please{" "}
         <NextUiLink
           isExternal
           size="sm"
           underline={"hover"}
-          className={"text-2xl"}
+          className={"text-xl md:text-2xl"}
           href={`mailto:${instructorEmailAddress}`}
         >
           let us know.
