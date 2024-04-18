13acbec4373f239655ae117276ad5540dc9e9988
Aaron Curtis <77994989+aaroncurtisyoga@users.noreply.github.com>
2024-04-18 18:58:00 -0400

    Create Store provider

---
+++ app/StoreProvider.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import type { AppStore } from "@/lib//redux/store";
+import { makeStore } from "@/lib/redux/store";
+import type { ReactNode } from "react";
+import { useRef } from "react";
+import { Provider } from "react-redux";
+
+interface Props {
+  readonly children: ReactNode;
+}
+
+export const StoreProvider = ({ children }: Props) => {
+  const storeRef = useRef<AppStore | null>(null);
+
+  if (!storeRef.current) {
+    // Create the store instance the first time this renders
+    storeRef.current = makeStore();
+  }
+
+  return <Provider store={storeRef.current}>{children}</Provider>;
+};
