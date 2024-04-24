c1c737e47e4168ed8c0a9160dd08ede05130c4fa
Aaron Curtis <77994989+aaroncurtisyoga@users.noreply.github.com>
2024-04-24 11:23:49 -0400

    Use router in details part of form

--- app/(admin)/events/create/details/page.tsx
+++ app/(admin)/events/create/details/page.tsx
@@ -1,17 +1,29 @@
-import { checkRole } from "@/lib/utils";
-import { redirect } from "next/navigation";
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import { useUser } from "@clerk/nextjs";
 import DetailsForInternallyHostedEvent from "@/components/events/EventForm/Steps/DetailsForInternallyHostedEvent";
 import DetailsForExternallyHostedEvent from "@/components/events/EventForm/Steps/DetailsForExternallyHostedEvent";
 import { useAppSelector } from "@/lib/redux/hooks";
 import { selectIsHostedExternally } from "@/lib/redux/features/eventFormSlice";
 
 const CreateEvent = () => {
+  const router = useRouter();
+  const { isSignedIn, isLoaded, user } = useUser();
   const isHostedExternally = useAppSelector(selectIsHostedExternally);
 
-  // If the user does not have the admin role, redirect them to the home page
-  if (!checkRole("admin")) {
-    redirect("/");
-  }
+  useEffect(() => {
+    if (!isLoaded) return;
+    if (isSignedIn) {
+      const isAdmin = user?.publicMetadata.role === "admin";
+      if (!isAdmin) {
+        router.push("/");
+      }
+    } else {
+      router.push("/sign-in");
+    }
+  }, [isLoaded, isSignedIn, router]);
 
   return (
     <section className={"wrapper"}>
