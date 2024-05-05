6f7b125182cca5ec970614a216272bceb81a81f1
Aaron Curtis <77994989+aaroncurtisyoga@users.noreply.github.com>
2024-05-05 18:40:53 -0400

    replace mongodb w/ prisma in stripe webhook

--- app/api/webhooks/stripe/route.ts
+++ app/api/webhooks/stripe/route.ts
@@ -1,7 +1,9 @@
 import { NextResponse } from "next/server";
 import stripe from "stripe";
-import { createOrder } from "@/lib/actions/order.actions";
+import { PrismaClient } from "@prisma/client";
 import { handleError } from "@/lib/utils";
+
+const prisma = new PrismaClient();
 
 export async function POST(request: Request) {
   const body = await request.text();
@@ -34,7 +36,7 @@ export async function POST(request: Request) {
     };
 
     try {
-      const newOrder = await createOrder(order);
+      const newOrder = await prisma.order.create({ data: order });
       return NextResponse.json({ message: "OK", order: newOrder });
     } catch (error) {
       handleError("Stripe Webhook Creating order", error);
