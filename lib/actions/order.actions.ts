75d05db3c745c6aee27de137f9bab8497842d174
Aaron Curtis <77994989+aaroncurtisyoga@users.noreply.github.com>
2024-05-08 11:48:10 -0400

    Reduce payload

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -111,7 +111,7 @@ export async function getOrdersByUser({
       orderBy: { createdAt: "asc" },
       skip: skipAmount,
       take: limit,
-      include: { event: { include: { category: true } } },
+      select: { event: { select: { title: true, id: true } } },
     });
 
     const totalOrders = await prisma.order.count({
