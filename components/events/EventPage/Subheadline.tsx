2be9d08040a4fb6a8a7bb146ad834c0e600dd9e0
Aaron Curtis <77994989+aaroncurtisyoga@users.noreply.github.com>
2024-05-07 20:17:26 -0400

    Move orders to admin route

--- components/events/EventPage/Subheadline.tsx
+++ components/events/EventPage/Subheadline.tsx
@@ -1,10 +1,9 @@
 import React, { FC } from "react";
-import { Event } from "@prisma/client";
 import ShareEvent from "@/components/events/EventPage/ShareEvent";
 import { formatDateTime } from "@/lib/utils";
 
 interface SubheadingProps {
-  category: { name: string };
+  category: string;
   id: string;
   startDateTime: Date;
 }
@@ -12,7 +11,7 @@ const Subheading: FC<SubheadingProps> = ({ category, id, startDateTime }) => {
   return (
     <div className="wrapper-width flex justify-between items-center w-full py-3">
       <p className={"text-base lg:text-lg font-semibold text-gray-600"}>
-        {formatDateTime(startDateTime).dateOnlyWithoutYear} • {category.name}
+        {formatDateTime(startDateTime).dateOnlyWithoutYear} • {category}
       </p>
       <ShareEvent eventId={id} />
     </div>
