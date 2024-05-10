546527dc6c6492361da44e8843a95f5f64582f47
Aaron Curtis <77994989+aaroncurtisyoga@users.noreply.github.com>
2024-05-10 19:22:07 -0400

    layout for admin dashboard

--- app/(admin)/layout.tsx
+++ app/(admin)/layout.tsx
@@ -1,11 +1,17 @@
 import type { Metadata } from "next";
 import { ReactNode } from "react";
+import Header from "@/components/shared/Header";
 
 export const metadata: Metadata = {
   title: "Admin",
 };
 const Layout = ({ children }: { children: ReactNode }) => {
-  return <div>{children}</div>;
+  return (
+    <div className={"flex flex-col min-h-dvh"}>
+      <Header />
+      {children}
+    </div>
+  );
 };
 
 export default Layout;
