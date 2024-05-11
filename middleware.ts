85dd65b07e63f998a17c71ba1166cec1892309a7
Aaron Curtis <77994989+aaroncurtisyoga@users.noreply.github.com>
2024-05-11 10:28:59 -0400

    start to project certain routes + add todo

--- middleware.ts
+++ middleware.ts
@@ -2,6 +2,18 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 export default clerkMiddleware();
 
+// todo: is it possible to add protected routes for things nested in (admin)
+//  path? like /admin/events/create but the /admin won't appear in the URl
+//  because its using Next.js route groups
+const isProtectedRoute = createRouteMatcher([
+  "/account",
+  "/dashboard",
+  "/profile",
+  "/settings",
+  "/events/create",
+  "/events/[id]/edit",
+  "/events/orders",
+]);
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
