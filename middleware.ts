eb3c164503011b8f3a5fb3b26dccccd18db4041e
Aaron Curtis <77994989+aaroncurtisyoga@users.noreply.github.com>
2024-05-12 20:32:27 -0400

    Update middleware

--- middleware.ts
+++ middleware.ts
@@ -16,10 +16,7 @@ export default clerkMiddleware((auth, req) => {
   // Restrict admin routes to users with specific permissions
   if (isAdminRoute(req)) {
     auth().protect((has) => {
-      return (
-        has({ permission: "org:sys_memberships:manage" }) ||
-        has({ permission: "org:sys_domains_manage" })
-      );
+      return has({ permission: "org:sys_memberships:manage" });
     });
   }
   // Restrict organization routes to users that are signed in
