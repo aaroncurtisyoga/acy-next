ff30058bee704d46ffc8de0501040efe7480feb9
Aaron Curtis <77994989+aaroncurtisyoga@users.noreply.github.com>
2024-05-17 13:25:15 -0400

    rmv (dashboard) dir and just create an /admin page

---
+++ app/admin/events/page.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react";
+
+const AdminEventsPage: FC = () => {
+  return (
+    <div>
+      <h1 className={"text-xl"}>Admin Events Page</h1>
+      <p>create event</p>
+      <p>events table w/ edit & delete buttons</p>
+    </div>
+  );
+};
+
+export default AdminEventsPage;
