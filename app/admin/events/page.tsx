d4d73887484d4fbff0ff9a6394f1722c48cde3ce
Aaron Curtis <77994989+aaroncurtisyoga@users.noreply.github.com>
2024-05-17 13:41:54 -0400

    Table for events

--- app/admin/events/page.tsx
+++ app/admin/events/page.tsx
@@ -1,16 +1,13 @@
-import Link from "next/link";
 import { FC } from "react";
-import { Button } from "@nextui-org/react";
-import { CirclePlus } from "lucide-react";
 import ButtonNewEvent from "@/app/admin/events/_components/ButtonNewEvent";
+import TableEventManagement from "@/app/admin/events/_components/TableEventManagement";
 
 const AdminEventsPage: FC = () => {
   return (
     <div className={"wrapper"}>
       <h1 className={"text-xl my-5"}>Admin Events Page</h1>
       <ButtonNewEvent />
-
-      <p>events table w/ edit & delete buttons</p>
+      <TableEventManagement />
     </div>
   );
 };
