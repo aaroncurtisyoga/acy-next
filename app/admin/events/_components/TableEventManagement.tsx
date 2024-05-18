3ee3c24ed2b358b27c57c35161e7f01ded596472
Aaron Curtis <77994989+aaroncurtisyoga@users.noreply.github.com>
2024-05-18 19:04:05 -0400

    Get table to list out events

--- app/admin/events/_components/TableEventManagement.tsx
+++ app/admin/events/_components/TableEventManagement.tsx
@@ -1,9 +1,64 @@
 "use client";
 
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableColumn,
+  TableHeader,
+  TableRow,
+} from "@nextui-org/react";
+import { Event } from "@prisma/client";
+import { getAllEvents } from "@/lib/actions/event.actions";
+import { handleError } from "@/lib/utils";
 
 const TableEventManagement: FC = () => {
-  return <div>table event mng</div>;
+  const [events, setEvents] = useState([]);
+  const [totalPages, setTotalPages] = useState(0);
+  const [page, setPage] = useState(1);
+  const [searchText, setSearchText] = useState("");
+  const [category, setCategory] = useState("");
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      try {
+        const { data: events, totalPages } = await getAllEvents({
+          category,
+          limit: 8,
+          page,
+          query: searchText,
+        });
+        setEvents(events);
+        setTotalPages(totalPages);
+      } catch (error) {
+        handleError("Failed to fetch events", error);
+      }
+    };
+    fetchEvents();
+  }, [page, searchText, category]);
+
+  return (
+    <>
+      <Table aria-label={"Table for Managing Events"} className={"mt-5"}>
+        <TableHeader>
+          <TableColumn>Start Date</TableColumn>
+          <TableColumn>Name</TableColumn>
+          <TableColumn>Category</TableColumn>
+        </TableHeader>
+        <TableBody>
+          {events.map((event: Event) => (
+            <TableRow key={event.id}>
+              <TableCell>{event.startDateTime.toDateString()}</TableCell>
+              <TableCell>{event.title}</TableCell>
+              <TableCell>{event.categoryId}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+      ;
+    </>
+  );
 };
 
 export default TableEventManagement;
--- app/admin/events/page.tsx
+++ app/admin/events/page.tsx
@@ -1,12 +1,10 @@
 import { FC } from "react";
-import ButtonNewEvent from "@/app/admin/events/_components/ButtonNewEvent";
 import TableEventManagement from "@/app/admin/events/_components/TableEventManagement";
 
 const AdminEventsPage: FC = () => {
   return (
     <div className={"wrapper"}>
       <h1 className={"text-xl my-5"}>Admin Events Page</h1>
-      <ButtonNewEvent />
       <TableEventManagement />
     </div>
   );
