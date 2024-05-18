f2afa1bc6331aff0d5c068b6c443d91d7f724369
Aaron Curtis <77994989+aaroncurtisyoga@users.noreply.github.com>
2024-05-18 19:22:17 -0400

    import type

--- components/events/Collection.tsx
+++ components/events/Collection.tsx
@@ -1,14 +1,9 @@
 import { FC } from "react";
-import { Category, Event, Location } from "@prisma/client";
 import EventCard from "@/components/events/EventCard";
 import EventText from "@/components/events/EventText";
 import Pagination from "@/components/events/Pagination";
 import NoEventsFound from "@/components/events/NoEventsFound";
-
-export type EventWithLocationAndCategory = Event & {
-  location: Location;
-  category: Category;
-};
+import { EventWithLocationAndCategory } from "@/types";
 
 interface CollectionProps {
   collectionType?: "Events_Organized" | "My_Tickets" | "All_Events";
