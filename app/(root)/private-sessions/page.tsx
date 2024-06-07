e4421b4caa283fa34da26b9aec91a305f2288f2e
Aaron Curtis <77994989+aaroncurtisyoga@users.noreply.github.com>
2024-06-06 20:24:34 -0400

    placeholder page

---
+++ app/(root)/private-sessions/page.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from "react";
+
+const PrivateSessions: FC = () => {
+  return <section className={"wrapper"}>private sessions placeholder</section>;
+};
+
+export default PrivateSessions;
