acc98f351dafc690fbd4d283c0a3a1b833ac7c1f
Aaron Curtis <77994989+aaroncurtisyoga@users.noreply.github.com>
2024-07-01 18:33:57 -0400

    begin styles for card

---
+++ app/(root)/private-sessions/_components/PurchaseCard.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import React from "react";
+import { Button, Card, CardBody, CardHeader } from "@nextui-org/react";
+import { CircleCheck } from "lucide-react";
+
+const PurchaseCard = () => {
+  return (
+    <Card className="max-w-[312px]">
+      <CardHeader>
+        <div className="flex flex-col">
+          <p className="text-xl font-semibold text-md">1 Session</p>
+          <p className="text-small text-default-500">1 hour of training</p>
+        </div>
+      </CardHeader>
+      <CardBody>
+        <p className={"text-4xl font-semibold"}>$125</p>
+        <Button
+          className={"text-base"}
+          fullWidth={true}
+          onPress={() => {
+            // Add purchase logic here
+            console.log("Purchase button clicked");
+          }}
+          color={"primary"}
+        >
+          Sign Up
+        </Button>
+
+        <p>This includes:</p>
+        <ul>
+          <li className={"flex"}>
+            <CircleCheck />
+            Personalized programming
+          </li>
+          <li className={"flex"}>
+            <CircleCheck />
+            Virtual or In Person
+          </li>
+          <li className={"flex"}>
+            <CircleCheck />
+            Breathwork
+          </li>
+          <li className={"flex"}>
+            <CircleCheck />
+            Meditation
+          </li>
+        </ul>
+      </CardBody>
+    </Card>
+  );
+};
+
+export default PurchaseCard;
