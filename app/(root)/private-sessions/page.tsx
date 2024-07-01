043b5f7e9338ef6d62c0d7336bf6b4f7d326a4ba
Aaron Curtis <77994989+aaroncurtisyoga@users.noreply.github.com>
2024-07-01 17:50:54 -0400

    header & headline

--- app/(root)/private-sessions/page.tsx
+++ app/(root)/private-sessions/page.tsx
@@ -1,7 +1,24 @@
 import React, { FC } from "react";
+import { Card, CardHeader, CardBody, CardFooter } from "@nextui-org/react";
 
 const PrivateSessions: FC = () => {
-  return <section className={"wrapper"}>private sessions placeholder</section>;
+  return (
+    <section className={"wrapper"}>
+      <div className="max-w-xl mx-auto">
+        <h1
+          className={"text-4xl font-bold mb-3 text-center mt-12 tracking-tight"}
+        >
+          Train With Me
+        </h1>
+        <p className={"text-center text-gray-500"}>
+          Whether you want to focus on a certain posture, or you&apos;re looking
+          for support in various aspects of your yoga journey, you can schedule
+          a single session or a group of sessions all catered towards your
+          personal goals.
+        </p>
+      </div>
+    </section>
+  );
 };
 
 export default PrivateSessions;
