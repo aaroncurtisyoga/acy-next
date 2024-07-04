d0842a303716a0c28f11d48e412bdf3b4c633db2
Aaron Curtis <77994989+aaroncurtisyoga@users.noreply.github.com>
2024-07-04 16:11:04 -0400

    Type & constants for Private offerings

---
+++ app/(root)/private-sessions/constants.ts
@@ -0,0 +1,56 @@
+import { OfferingType } from "@/app/(root)/private-sessions/types";
+
+export const INDIVIDUAL_OFFERINGS: OfferingType[] = [
+  {
+    title: "1 Session",
+    price: "$125",
+    description: "1 hour of training",
+    includes: [
+      "Personalized programming",
+      "Virtual or In Person",
+      "Breathwork",
+      "Meditation",
+    ],
+  },
+  {
+    title: "4 Sessions",
+    price: "$450",
+    description: "4 hours of training",
+    includes: [
+      "Personalized programming",
+      "Virtual or In Person",
+      "Breathwork",
+      "Meditation",
+      "Async Q & A",
+      "Video Support",
+    ],
+  },
+];
+
+export const GROUP_OFFERINGS: OfferingType[] = [
+  {
+    title: "1 Session",
+    price: "$200",
+    description: "1 hour of training",
+    includes: [
+      "Unique programming for your group",
+      "Virtual or In Person",
+      "Breathwork",
+      "Meditation",
+    ],
+  },
+  {
+    title: "4 Sessions",
+    price: "$700",
+    description: "4 hours of training",
+    includes: [
+      "Unique programming for your group",
+      "Virtual or In Person",
+      "Breathwork",
+      "Meditation",
+      "Async Q & A",
+      "Video Support",
+      "Sound Bath",
+    ],
+  },
+];
---
+++ app/(root)/private-sessions/types.ts
@@ -0,0 +1,6 @@
+export interface OfferingType {
+  title: string;
+  price: string;
+  description: string;
+  includes: string[];
+}
