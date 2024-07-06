b6d91a837dc24bfa0cb005ba103a98883809ff1a
Aaron Curtis <77994989+aaroncurtisyoga@users.noreply.github.com>
2024-07-06 13:03:09 -0400

    adjust text

--- app/(root)/private-sessions/page.tsx
+++ app/(root)/private-sessions/page.tsx
@@ -25,7 +25,7 @@ const PrivateSessions: FC = () => {
           Whether you want to focus on a certain posture, or you&apos;re looking
           for support in various aspects of your yoga journey, you can schedule
           a single session or a group of sessions all catered towards your
-          personal goals.
+          goals.
         </p>
       </div>
       <GroupSizeTabs setPrivateSessionType={setPrivateSessionType} />
