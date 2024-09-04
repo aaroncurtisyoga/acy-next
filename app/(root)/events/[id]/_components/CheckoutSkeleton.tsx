e3f92204b8aa3bfad2c0cb4029f59cd0ac3aedce
Aaron Curtis <77994989+aaroncurtisyoga@users.noreply.github.com>
2024-09-04 12:29:39 -0400

    have all the stuff inside checkout show at same time. bc its weird seeing price w/o seeing the btn

--- app/(root)/events/[id]/_components/CheckoutSkeleton.tsx
+++ app/(root)/events/[id]/_components/CheckoutSkeleton.tsx
@@ -3,9 +3,25 @@ import React from "react";
 
 const CheckoutSkeleton: React.FC = () => {
   return (
-    <Skeleton className={"rounded-xl"}>
-      <Button type="button" fullWidth={true} color={"primary"}></Button>
-    </Skeleton>
+    <div
+      className={
+        "flex-1 w-full border-t-2 h-[140px] p-[24px] fixed bottom-0 z-10 bg-white" +
+        " md:border-[1px] md:rounded-2xl md:relative" +
+        " md:max-w-[360px]"
+      }
+    >
+      <Skeleton className={"mb-3"}>
+        <p className={"text-center text-lg"}>$</p>
+      </Skeleton>
+      <Skeleton>
+        <Button
+          type="button"
+          fullWidth={true}
+          color={"primary"}
+          className={"rounded-xl"}
+        ></Button>
+      </Skeleton>
+    </div>
   );
 };
 
