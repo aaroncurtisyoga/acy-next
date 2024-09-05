0b8f5196e9d5dc4b423b6bd453bbb4be9b07bd02
Aaron Curtis <77994989+aaroncurtisyoga@users.noreply.github.com>
2024-09-05 11:34:53 -0400

    add 2 fonts. for headers (e.g., Big Titles, Event Names) Playfair Display. Body Text (e.g., Paragraphs, Descriptions, Basic Typography): Roboto Flex

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,14 @@
 import type { Metadata } from "next";
 import { ReactNode } from "react";
 import { Analytics } from "@vercel/analytics/react";
+import { Playfair_Display, Roboto_Flex } from "next/font/google";
 import { Providers } from "@/app/providers";
 import "./globals.css";
+
+const playfairDisplay = Playfair_Display({
+  subsets: ["latin"],
+});
+const robotoFlex = Roboto_Flex({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: { template: "%s | Aaron Curtis Yoga", default: "Aaron Curtis Yoga" },
@@ -11,7 +17,10 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en">
+    <html
+      lang="en"
+      className={`${playfairDisplay.className} ${robotoFlex.className}`}
+    >
       <body>
         <Providers>
           {children}
