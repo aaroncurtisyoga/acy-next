f0025513d156cdb916ce9cd82f28001ede5320aa
Aaron Curtis <77994989+aaroncurtisyoga@users.noreply.github.com>
2024-09-05 11:43:23 -0400

    move fonts to its own file

---
+++ app/fonts.ts
@@ -0,0 +1,7 @@
+import { Playfair_Display, Roboto_Flex } from "next/font/google";
+
+export const playfairDisplay = Playfair_Display({
+  subsets: ["latin"],
+});
+
+export const robotoFlex = Roboto_Flex({ subsets: ["latin"] });
