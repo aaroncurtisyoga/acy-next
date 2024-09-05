553ddd7c54c81c2287a37d7322520bca48dd3ad1
Aaron Curtis <77994989+aaroncurtisyoga@users.noreply.github.com>
2024-09-05 10:06:48 -0400

    comment out newsletter for now

--- _components/Footer.tsx
+++ _components/Footer.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import { Link as NextUiLink } from "@nextui-org/react";
 
 import { AudioLines, Instagram, Youtube } from "lucide-react";
-import NewsletterForm from "@/_components/NewsletterForm";
+// import NewsletterForm from "@/_components/NewsletterForm";
 import { instructorEmailAddress } from "@/_lib/constants";
 
 const Footer = () => {
@@ -15,7 +15,8 @@ const Footer = () => {
         }
       >
         <div className={"flex justify-center"}>
-          <NewsletterForm />
+          <p>Newsletter coming soon...</p>
+          {/*<NewsletterForm />*/}
         </div>
         <div className={"flex justify-center"}>
           <Link
