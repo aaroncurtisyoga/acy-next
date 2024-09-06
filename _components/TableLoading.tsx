e01e6bfbac0c464694b9bc611ea2876e5d064408
Aaron Curtis <77994989+aaroncurtisyoga@users.noreply.github.com>
2024-09-06 12:39:20 -0400

    create table components to handle these states: Loading, IsEmpty

---
+++ _components/TableLoading.tsx
@@ -0,0 +1,27 @@
+import React, { FC } from "react";
+import {
+  Table,
+  TableBody,
+  TableColumn,
+  TableHeader,
+  Spinner,
+} from "@nextui-org/react";
+
+interface EmptyTableProps {
+  columns: Array<string>;
+}
+
+const TableLoading: FC<EmptyTableProps> = ({ columns }) => {
+  return (
+    <Table>
+      <TableHeader>
+        {columns.map((column, index) => (
+          <TableColumn key={index}>{column}</TableColumn>
+        ))}
+      </TableHeader>
+      <TableBody emptyContent={<Spinner />}>{[]}</TableBody>
+    </Table>
+  );
+};
+
+export default TableLoading;
