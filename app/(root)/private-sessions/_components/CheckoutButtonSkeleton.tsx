9e74477e995ad7cb98a974b4309ec70c9a14eac2
Aaron Curtis <77994989+aaroncurtisyoga@users.noreply.github.com>
2024-09-07 12:54:37 -0400

    update styles to match new button ui

--- app/(root)/private-sessions/_components/CheckoutButtonSkeleton.tsx
+++ app/(root)/private-sessions/_components/CheckoutButtonSkeleton.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const CheckoutSkeleton: React.FC = () => {
   return (
-    <Skeleton className={"rounded-xl"}>
+    <Skeleton className={"w-full max-w-[440px] mx-auto mt-10 mb-40 rounded-xl"}>
       <Button fullWidth={true}></Button>
     </Skeleton>
   );
