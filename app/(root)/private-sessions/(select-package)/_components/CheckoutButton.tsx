79fd46f0df32adb02f1b61e8811d26fe2319806c
Aaron Curtis <77994989+aaroncurtisyoga@users.noreply.github.com>
2024-09-22 20:32:04 -0400

    remove unused import

--- app/(root)/private-sessions/(select-package)/_components/CheckoutButton.tsx
+++ app/(root)/private-sessions/(select-package)/_components/CheckoutButton.tsx
@@ -1,16 +1,12 @@
 "use client";
 
-import React, { FC, FormEvent } from "react";
+import React, { FC } from "react";
 import { SignedIn, SignedOut, SignInButton, useUser } from "@clerk/nextjs";
 import { Button } from "@nextui-org/react";
-import { OrderType } from "@prisma/client";
-import { loadStripe } from "@stripe/stripe-js";
 import CheckoutButtonSkeleton from "@/app/(root)/private-sessions/(select-package)/_components/CheckoutButtonSkeleton";
 
-loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY);
-
 const CheckoutButton: FC = () => {
-  const { user, isLoaded: isUserLoaded } = useUser();
+  const { isLoaded: isUserLoaded } = useUser();
 
   if (!isUserLoaded) {
     return <CheckoutButtonSkeleton />;
