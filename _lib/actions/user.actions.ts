4476a7ffd90b7298e1c0d5943a9ad23e44e71cd9
Aaron Curtis <77994989+aaroncurtisyoga@users.noreply.github.com>
2024-09-29 19:12:37 -0400

    start user action

---
+++ _lib/actions/user.actions.ts
@@ -0,0 +1,21 @@
+import { handleError } from "@/_lib/utils";
+import prisma from "@/_lib/prisma";
+
+export async function getAllUsers({ query, limit = 8, page }) {
+  const skipAmount = (Number(page) - 1) * limit;
+  try {
+    const users = await prisma.user.findMany({
+      // Todo: Setup to work w/ search query
+      where: {},
+      orderBy: { firstName: "asc" },
+      take: limit,
+      skip: skipAmount,
+    });
+    return {
+      data: users,
+      totalPage: Math.ceil(users.length / limit),
+    };
+  } catch (error) {
+    handleError(error);
+  }
+}
