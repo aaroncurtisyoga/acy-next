c038578a0fc1ca4d892d047046c7c7a024676e81
Aaron Curtis <77994989+aaroncurtisyoga@users.noreply.github.com>
2024-10-26 14:41:59 -0400

    import order

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,11 +1,11 @@
+import path from "path";
+import dotenv from "dotenv";
 import { defineConfig, devices } from "@playwright/test";
 
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-import dotenv from "dotenv";
-import path from "path";
 dotenv.config({ path: path.resolve(__dirname, ".env") });
 
 /**
