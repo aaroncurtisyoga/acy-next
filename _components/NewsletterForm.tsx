9ce05907b4e7165e9eca0e7e7e9adb5c18331000
Aaron Curtis <77994989+aaroncurtisyoga@users.noreply.github.com>
2024-11-30 15:12:55 -0500

    bring newsletter form bck to main

--- _components/NewsletterForm.tsx
+++ _components/NewsletterForm.tsx
@@ -1,7 +1,9 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Input } from "@nextui-org/react";
+import { Input, Spinner } from "@nextui-org/react";
+import { ArrowRight, Check } from "lucide-react";
 import { SubmitHandler, useForm, Controller } from "react-hook-form";
 import { z } from "zod";
 import { addNewsletterEntry } from "@/_lib/actions/newsletter.actions";
@@ -13,6 +15,7 @@ const NewsletterForm = () => {
   const {
     control,
     handleSubmit,
+    reset,
     setError,
     formState: { errors, isSubmitting, isSubmitSuccessful },
   } = useForm<Inputs>({
@@ -21,6 +24,15 @@ const NewsletterForm = () => {
       email: "",
     },
   });
+  const [resetFormOnNextInput, setResetFormOnNextInput] = useState(false);
+
+  useEffect(() => {
+    // Flag form to reset on next input. Done this way so success message
+    // can persist until the user starts typing again.
+    if (isSubmitSuccessful) {
+      setResetFormOnNextInput(true);
+    }
+  }, [reset, isSubmitSuccessful]);
 
   const handleErrorsFromServerSideValidation = (
     formErrors: z.ZodFormattedError<typeof newsletterFormSchema>,
@@ -46,6 +58,18 @@ const NewsletterForm = () => {
     });
   };
 
+  const determineFormIcon = (
+    isSubmitting: boolean,
+    isSubmitSuccessful: boolean,
+  ) => {
+    if (isSubmitting) {
+      return <Spinner size={"sm"} />;
+    }
+    if (isSubmitSuccessful) {
+      return <Check className={"text-success"} />;
+    }
+    return <ArrowRight className={"text-default-900"} />;
+  };
   const onSubmit: SubmitHandler<Inputs> = async (data) => {
     const result = await addNewsletterEntry(data);
     if (result.formErrors) {
@@ -61,7 +85,67 @@ const NewsletterForm = () => {
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <p>form will go here</p>
+      <Controller
+        name={"email"}
+        control={control}
+        rules={{
+          required: true,
+        }}
+        render={({ field }) => (
+          <Input
+            classNames={{
+              description: "text-default-900",
+              label: "font-medium",
+              mainWrapper: "w-64",
+            }}
+            description={`${
+              isSubmitSuccessful
+                ? "Thank you for signing up!"
+                : "Be the first to know about events & more!"
+            }`}
+            type={"email"}
+            label={"Newsletter:"}
+            labelPlacement="outside"
+            variant="underlined"
+            name={"email"}
+            placeholder={"Email"}
+            value={field.value}
+            onBeforeInput={() => {
+              if (resetFormOnNextInput) {
+                reset({
+                  email: "",
+                });
+                setResetFormOnNextInput(false);
+              }
+            }}
+            onChange={(e) => {
+              field.onChange(e);
+            }}
+            isDisabled={isSubmitting}
+            errorMessage={
+              (errors.email && errors.email.message) ||
+              (errors.root && errors.root.message)
+            }
+            endContent={
+              <button
+                className="focus:outline-none"
+                type="submit"
+                disabled={isSubmitting}
+                aria-label={
+                  isSubmitting
+                    ? "Submitting"
+                    : isSubmitSuccessful
+                      ? "Submission successful"
+                      : "Submit newsletter form"
+                }
+              >
+                {determineFormIcon(isSubmitting, isSubmitSuccessful)}
+              </button>
+            }
+            {...field}
+          />
+        )}
+      />
     </form>
   );
 };
