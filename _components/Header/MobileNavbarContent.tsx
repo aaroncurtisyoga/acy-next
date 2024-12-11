bca9cb351da4a5f81c0a48227973506671163e65
Aaron Curtis <77994989+aaroncurtisyoga@users.noreply.github.com>
2024-12-11 18:39:46 -0500

    Create AuthButtons component

--- _components/Header/MobileNavbarContent.tsx
+++ _components/Header/MobileNavbarContent.tsx
@@ -1,15 +1,14 @@
 "use client";
 
-import { FC, useEffect } from "react";
+import { FC } from "react";
 import Link from "next/link";
-import { usePathname, useRouter } from "next/navigation";
-import { SignedIn, SignedOut, useClerk } from "@clerk/nextjs";
 import {
   NavbarContent,
   NavbarMenu,
   NavbarMenuItem,
   NavbarMenuToggle,
 } from "@nextui-org/react";
+import AuthButtons from "@/_components/Header/AuthButtons";
 
 interface MobileNavbarContentProps {
   isMenuOpen: boolean;
@@ -22,10 +21,6 @@ const MobileNavbarContent: FC<MobileNavbarContentProps> = ({
   menuItems,
   setIsMenuOpen,
 }) => {
-  const router = useRouter();
-  const pathname = usePathname();
-  const { signOut } = useClerk();
-
   return (
     <>
       <NavbarContent className="sm:hidden" justify="end">
@@ -78,23 +73,7 @@ const MobileNavbarContent: FC<MobileNavbarContentProps> = ({
           data-testid="menu-login"
           className="py-3 px-4 w-full text-right border-b border-gray-400 hover:bg-gray-100 focus:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-primary"
         >
-          <SignedIn>
-            <button
-              type="button"
-              onClick={() => signOut(() => router.push("/"))}
-              className="w-full text-lg font-medium text-gray-800 text-right"
-            >
-              Logout
-            </button>
-          </SignedIn>
-          <SignedOut>
-            <Link
-              href={"/sign-in"}
-              className="block w-full text-lg font-medium text-gray-800"
-            >
-              Login
-            </Link>
-          </SignedOut>
+          <AuthButtons />
         </NavbarMenuItem>
       </NavbarMenu>
     </>
