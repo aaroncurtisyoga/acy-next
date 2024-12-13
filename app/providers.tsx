a10a1bb3a9fbbe8b720b0d0392b25dfd83c42cc9
Aaron Curtis <77994989+aaroncurtisyoga@users.noreply.github.com>
2024-12-13 17:09:00 -0500

    use interface

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,11 +1,16 @@
 "use client";
 
+import { ReactNode } from "react";
 import { useRouter } from "next/navigation";
-import { ClerkProvider } from "@clerk/nextjs";
+import { ClerkProvider, useUser } from "@clerk/nextjs";
 import { NextUIProvider } from "@nextui-org/react";
 import { StoreProvider } from "@/app/StoreProvider";
 
-export function Providers({ children }: { children: React.ReactNode }) {
+interface ProvidersProps {
+  children: ReactNode;
+}
+
+export function Providers({ children }: ProvidersProps) {
   const router = useRouter();
 
   return (
