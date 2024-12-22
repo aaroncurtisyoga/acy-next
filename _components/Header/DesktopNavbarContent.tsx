654704bf3ecbe4225b607f30d5d7275f70fc0ef2
Aaron Curtis <77994989+aaroncurtisyoga@users.noreply.github.com>
2024-12-21 20:03:03 -0500

    start to address ts error due to next ui bug

--- _components/Header/DesktopNavbarContent.tsx
+++ _components/Header/DesktopNavbarContent.tsx
@@ -2,22 +2,14 @@
 
 import { FC } from "react";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
-import { usePathname } from "next/navigation";
+import { useRouter, usePathname } from "next/navigation";
 import { useClerk, useUser } from "@clerk/nextjs";
-import {
-  NavbarContent,
-  NavbarItem,
-  Dropdown,
-  DropdownMenu,
-  DropdownItem,
-  DropdownTrigger,
-} from "@nextui-org/react";
-import { User } from "lucide-react";
+import { NavbarContent, NavbarItem } from "@nextui-org/react";
+import UserDropdownMenu from "@/_components/Header/UserDropdownMenu";
 import { unauthenticatedLinks } from "@/_lib/constants";
 
 interface DesktopNavbarContentProps {
-  authenticatedLinks: { name: string; href: string; testId: string }[];
+  authenticatedLinks: any[];
 }
 
 const DesktopNavbarContent: FC<DesktopNavbarContentProps> = ({
@@ -28,10 +20,9 @@ const DesktopNavbarContent: FC<DesktopNavbarContentProps> = ({
   const pathname = usePathname();
   const { signOut } = useClerk();
 
-  // @ts-ignore
   return (
     <NavbarContent className="hidden sm:flex gap-4" justify="end">
-      {/* Auth and Unauthenticated Links */}
+      {/* Unauthenticated Links */}
       {unauthenticatedLinks.map((link, index) => (
         <NavbarItem
           data-testid={`navbar-item-${link.testId}`}
@@ -43,34 +34,13 @@ const DesktopNavbarContent: FC<DesktopNavbarContentProps> = ({
           </Link>
         </NavbarItem>
       ))}
-      <Dropdown placement="bottom-end">
-        <DropdownTrigger>
-          <button aria-label="User menu" className="flex items-center">
-            <User className="w-6 h-6" />
-          </button>
-        </DropdownTrigger>
-        <DropdownMenu aria-label="User Menu" variant="flat">
-          {/* ts-ignore */}
-          {authenticatedLinks.map((link) => (
-            <DropdownItem href={link.href} title={link.name} key={link.name}>
-              {link.name}
-            </DropdownItem>
-          ))}
-          {isSignedIn ? (
-            <DropdownItem
-              key="logout"
-              title={"Log out"}
-              onPress={() => signOut(() => router.push("/"))}
-            >
-              Log out
-            </DropdownItem>
-          ) : (
-            <DropdownItem key="login" title={"Log In"} href="/sign-in">
-              Log in
-            </DropdownItem>
-          )}
-        </DropdownMenu>
-      </Dropdown>
+
+      {/* Dropdown for Authenticated Links */}
+      <UserDropdownMenu
+        authenticatedLinks={authenticatedLinks}
+        isSignedIn={isSignedIn}
+        signOut={() => signOut(() => router.push("/"))}
+      />
     </NavbarContent>
   );
 };
