d45f606af554ef73ec070b53544afe2f15d5d34b
Aaron Curtis <77994989+aaroncurtisyoga@users.noreply.github.com>
2025-03-21 13:19:56 -0400

    Refactor Playwright config and update global setup logic

--- e2e/global.setup.ts
+++ e2e/global.setup.ts
@@ -1,8 +1,7 @@
 import { clerkSetup } from "@clerk/testing/playwright";
-import { test as setup } from "@playwright/test";
 
-setup.describe.configure({ mode: "serial" });
-
-setup("global setup", async ({}) => {
+async function globalSetup() {
   await clerkSetup();
-});
+}
+
+export default globalSetup;
