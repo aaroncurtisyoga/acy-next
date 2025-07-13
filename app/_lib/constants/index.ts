7adf6f4e1f203d95abe062f4e516dc662b042f4f
Aaron Curtis <77994989+aaroncurtisyoga@users.noreply.github.com>
2025-07-13 09:39:39 -0400

    refactor: remove About page and its associated link from constants for a cleaner navigation structure

--- app/_lib/constants/index.ts
+++ app/_lib/constants/index.ts
@@ -13,11 +13,6 @@ import { TravelOption } from "@/app/_lib/types";
 
 export const unauthenticatedLinks = [
   {
-    name: "About",
-    href: "/about",
-    testId: "about-link",
-  },
-  {
     name: "Private Sessions",
     href: "/private-sessions",
     testId: "private-sessions-link",
